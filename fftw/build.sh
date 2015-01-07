f4b86101824e01e134ad24e2fc1b4c5ee46cb965
Stuart Berg <bergs@janelia.hhmi.org>
2015-01-07 15:22:27 -0500

    fftw/build.sh: Use -L in LDFLAGS. Also: slight refactoring.

--- fftw/build.sh
+++ fftw/build.sh
@@ -2,28 +2,43 @@
 # inspired by build script for Arch Linux fftw pacakge:
 # https://projects.archlinux.org/svntogit/packages.git/tree/trunk/PKGBUILD?h=packages/fftw
 
+# Get commonly needed env vars
+CWD=$(cd `dirname $0` && pwd)
+source $CWD/../common-vars.sh
+
+export LDFLAGS="-L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -I${PREFIX}/include"
+
 CONFIGURE="./configure --prefix=$PREFIX --enable-shared --enable-threads --disable-fortran"
 
-# Single precision (fftw libraries have "f" suffix)
-$CONFIGURE --enable-float --enable-sse
-make -j${CPU_COUNT}
-make install
-
-# Long double precision (fftw libraries have "l" suffix)
-$CONFIGURE --enable-long-double
-make -j${CPU_COUNT}
-make install
-
-# Double precision (fftw libraries have no precision suffix)
-$CONFIGURE --enable-sse2
-make -j${CPU_COUNT}
-make install
+# (Note exported LDFLAGS and CFLAGS vars provided above.)
+BUILD_CMD="make -j${CPU_COUNT}"
+INSTALL_CMD="make install"
 
 # Test suite
 # tests are performed during building as they are not available in the
 # installed package.
-# Additional tests can be run with make smallcheck and make bigcheck
-cd tests && make check-local
-# Additional tests can be run using the next two lines
-#make smallcheck
-#make bigcheck
+# Additional tests can be run with "make smallcheck" and "make bigcheck"
+TEST_CMD="eval cd tests && ${LIBRARY_SEARCH_VAR}=\"$PREFIX/lib\" make check-local && cd -"
+
+#
+# We build 3 different versions of fftw:
+#
+
+# (1) Single precision (fftw libraries have "f" suffix)
+$CONFIGURE --enable-float --enable-sse
+${BUILD_CMD}
+${INSTALL_CMD}
+${TEST_CMD}
+
+# (2) Long double precision (fftw libraries have "l" suffix)
+$CONFIGURE --enable-long-double
+${BUILD_CMD}
+${INSTALL_CMD}
+${TEST_CMD}
+
+# (3) Double precision (fftw libraries have no precision suffix)
+$CONFIGURE --enable-sse2
+${BUILD_CMD}
+${INSTALL_CMD}
+${TEST_CMD}
