bf4c6e2702edc260b1c9eb3c818a12f57caec09b
Stuart Berg <bergs@janelia.hhmi.org>
2015-01-09 15:29:29 -0500

    boost: Fixed linker argument to b2 if there are no LDFLAGS.

--- boost/build.sh
+++ boost/build.sh
@@ -27,14 +27,22 @@ mkdir -vp ${PREFIX}/bin;
   --with-libraries=date_time,filesystem,python,regex,serialization,system,test,thread,program_options,chrono,atomic,random \
   --with-python=${PYTHON} \
   --prefix=${PREFIX}
-  
+
+LINK_ARG=""
+if [ "${CXX_LDFLAGS}" != "" ]; then
+    LINK_ARG=linkflags=
+fi
+
+echo "LINK_ARG=$LINK_ARG"
+
 # First, with --layout=tagged to create libraries named with -mt convention
 ./b2 \
   --layout=tagged \
   -j ${CPU_COUNT} \
   -sNO_BZIP2=1 \
   ${B2ARGS} \
-  cxxflags="${CXXFLAGS}" linkflags="${CXX_LDFLAGS}" \
+  cxxflags="${CXXFLAGS}" \
+  ${LINK_ARG}"${CXX_LDFLAGS}" \
   install
 
 # Second, without --layout=tagged, to create libraries without -mt names
@@ -43,44 +51,10 @@ mkdir -vp ${PREFIX}/bin;
   -j ${CPU_COUNT} \
   -sNO_BZIP2=1 \
   ${B2ARGS} \
-  cxxflags="${CXXFLAGS}" linkflags="${CXX_LDFLAGS}" \
+  cxxflags="${CXXFLAGS}" \
+  ${LINK_ARG}"${CXX_LDFLAGS}" \
   install
 
 # Omitted these options from above commands:  
 #  -sZLIB_INCLUDE=${PREFIX}/include \
 #  -sZLIB_SOURCE=${zlib_SRC_DIR} \
-
-
-#if [ `uname` == Darwin ]; then
-#
-#  MACOSX_VERSION_MIN=10.8
-#  CXXFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
-#  CXXFLAGS="${CXXFLAGS} -std=c++11 -stdlib=libc++"
-#  LINKFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN} "
-#  LINKFLAGS="${LINKFLAGS} -stdlib=libc++"
-#
-#  B2ARGS="toolset=clang"
-#
-#  ./bootstrap.sh \
-#    --prefix="${PREFIX}/" --libdir="${PREFIX}/lib/" \
-#    | tee bootstrap.log 2>&1
-#  ./b2 \
-#    variant=release address-model=64 architecture=x86 \
-#    threading=multi link=shared ${B2ARGS} \
-#    cxxflags="${CXXFLAGS}" linkflags="${LINKFLAGS}" \
-#    install | tee b2.log 2>&1
-#
-#else
-#  echo "FIXME: Boost build.sh doesn't handle Linux"
-#  exit 1
-#  B2ARGS="toolset=gcc"
-#
-#  ./bootstrap.sh \
-#    --prefix="${PREFIX}/" --libdir="${PREFIX}/lib/" \
-#    | tee bootstrap.log 2>&1
-#  ./b2 \
-#    variant=release address-model=64 architecture=x86 \
-#    threading=multi link=shared ${B2ARGS} \
-#    install | tee b2.log 2>&1
-#fi
-
