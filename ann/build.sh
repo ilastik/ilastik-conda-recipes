62d115f8db3fc7f84a665bc0c42ce9abb8dbef8b
Carsten Haubold <carstenhaubold@googlemail.com>
2015-02-12 17:45:47 +0100

    Adding ANN config

---
+++ ann/build.sh
@@ -0,0 +1,10 @@
+mkdir build
+cd build
+cmake ..\
+	-DCMAKE_OSX_DEPLOYMENT_TARGET=10.7\
+	-DCMAKE_INSTALL_PREFIX=${PREFIX}\
+	-DCMAKE_PREFIX_PATH=${PREFIX}\
+	-DBUILD_SHARED_LIBS=ON
+
+make -j${CPU_COUNT}
+make install
