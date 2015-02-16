605521aaa71bbdbd67d3458c1b7081090ae123ac
Carsten Haubold <carstenhaubold@googlemail.com>
2015-02-16 10:25:39 +0100

    insert OSX cplex lib rpath changes

--- pgmlink/build.sh
+++ pgmlink/build.sh
@@ -1,3 +1,5 @@
+export CPLEX_DIR=/Users/chaubold/hci/code/cplex
+
 mkdir build
 cd build
 cmake ..\
@@ -9,7 +11,18 @@ cmake ..\
     -DWITH_PYTHON=ON\
     -DWITH_CHECKED_STL=OFF\
     -DWITH_TESTS=ON\
-    -DCPLEX_ROOT_DIR=/Users/chaubold/hci/code/cplex
+    -DCPLEX_ROOT_DIR=${CPLEX_DIR}
 
 make -j${CPU_COUNT}
 make install
+
+if [ `uname` == Darwin ]; then
+	install_name_tool -change ${CPLEX_DIR}/cplex/lib/x86-64_osx/static_pic/libcplex.dylib @loader_path/./libcplex.dylib ${PREFIX}/lib/libpgmlink.dylib
+	install_name_tool -change ${CPLEX_DIR}/cplex/lib/x86-64_osx/static_pic/libilocplex.dylib @loader_path/./libilocplex.dylib ${PREFIX}/lib/libpgmlink.dylib
+	install_name_tool -change ${CPLEX_DIR}/concert/lib/x86-64_osx/static_pic/libconcert.dylib @loader_path/./libconcert.dylib ${PREFIX}/lib/libpgmlink.dylib
+	install_name_tool -change ${CPLEX_DIR}/cplex/lib/x86-64_osx/static_pic/libcplex.dylib @loader_path/./libcplex.dylib ${PREFIX}/lib/python2.7/site-packages/pgmlink.so
+	install_name_tool -change ${CPLEX_DIR}/cplex/lib/x86-64_osx/static_pic/libilocplex.dylib @loader_path/./libilocplex.dylib ${PREFIX}/lib/python2.7/site-packages/pgmlink.so
+	install_name_tool -change ${CPLEX_DIR}/concert/lib/x86-64_osx/static_pic/libconcert.dylib @loader_path/./libconcert.dylib ${PREFIX}/lib/python2.7/site-packages/pgmlink.so
+else
+    echo "Use chrpath here!"
+fi
