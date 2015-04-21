6c1513043c28c0c076cdac642c145c5409272f2f
Stuart Berg <bergs@janelia.hhmi.org>
2015-04-21 19:16:59 -0400

    Added jsoncpp

---
+++ jsoncpp/build.sh
@@ -0,0 +1,30 @@
+# Get commonly needed env vars
+CWD=$(cd `dirname $0` && pwd)
+source $CWD/../common-vars.sh
+
+# CONFIGURE
+mkdir build
+cd build
+cmake ..\
+        -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+        -DCMAKE_PREFIX_PATH=${PREFIX} \
+        -DCMAKE_CXX_FLAGS="-fPIC"
+        -DBUILD_SHARED_LIBS=1 \
+        -DCMAKE_SHARED_LINKER_FLAGS=-L${PREFIX}/lib \
+        -DCMAKE_EXE_LINKER_FLAGS=-L${PREFIX}/lib \
+        -DPYTHON_EXECUTABLE=${PYTHON} \
+
+# BUILD
+make -j${CPU_COUNT}
+
+
+# TEST (before install)
+(
+    # (Since conda hasn't performed its link step yet, we must 
+    #  help the tests locate their dependencies via LD_LIBRARY_PATH)
+    export ${LIBRARY_SEARCH_VAR}=$PREFIX/lib:${!LIBRARY_SEARCH_VAR}
+    make jsoncpp_check
+)
+
+# "install" to the build prefix (conda will relocate these files afterwards)
+make install
