4ac7e3acc352921b7d992962f90666484fa3eab1
Stuart Berg <bergs@janelia.hhmi.org>
2015-05-04 19:23:55 -0400

    Added vtk, can remove once conda/conda-recipes#287 is merged and included in Anaconda.

---
+++ vtk/run_test.py
@@ -0,0 +1,19 @@
+import vtk.vtkChartsPython
+import vtk.vtkCommonPython
+import vtk.vtkFilteringPython
+import vtk.vtkGenericFilteringPython
+import vtk.vtkGeovisPython
+import vtk.vtkGraphicsPython
+import vtk.vtkHybridPython
+import vtk.vtkIOPython
+import vtk.vtkImagingPython
+import vtk.vtkInfovisPython
+import vtk.vtkRenderingPython
+import vtk.vtkViewsPython
+import vtk.vtkVolumeRenderingPython
+import vtk.vtkWidgetsPython
+
+# Qt extension
+import vtk.QVTKWidget
+import vtk.QVTKWidget2
+import vtk.vtkQImageToImageSource
