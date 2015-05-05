bb1236b7656d02616c0f94d90e922ec93a69ef85
Stuart Berg <bergs@janelia.hhmi.org>
2015-05-05 19:28:55 -0400

    pgmlink: - Linux: Fix RPATH of output binaries - Perform RPATH fix via post-link.sh - Use CPLEX_ROOT_DIR from the user's environment, at both build and install time.

--- pgmlink/build.sh
+++ pgmlink/build.sh
@@ -1,4 +1,10 @@
-export CPLEX_DIR=/Users/chaubold/hci/code/cplex
+if [ -z "$CPLEX_ROOT_DIR" ]; then
+    echo "******************************************"
+    echo "* You must define CPLEX_ROOT_DIR in your *"
+    echo "* environment before building pgmlink.   *"
+    echo "******************************************"
+    exit 1
+fi
 
 mkdir build
 cd build
@@ -6,23 +12,13 @@ cmake ..\
 	-DCMAKE_OSX_DEPLOYMENT_TARGET=10.7\
 	-DCMAKE_INSTALL_PREFIX=${PREFIX}\
 	-DCMAKE_PREFIX_PATH=${PREFIX}\
+    -DCMAKE_SHARED_LINKER_FLAGS=${LDFLAGS}\ -Wl,-rpath,${PREFIX}/lib\ -L${PREFIX}/lib \
+    -DCMAKE_EXE_LINKER_FLAGS=${LDFLAGS}\ -Wl,-rpath,${PREFIX}/lib\ -L${PREFIX}/lib \
 	-DBUILD_SHARED_LIBS=ON\
-    -DWITH_HDF5=ON\
     -DWITH_PYTHON=ON\
     -DWITH_CHECKED_STL=OFF\
     -DWITH_TESTS=ON\
-    -DCPLEX_ROOT_DIR=${CPLEX_DIR}
+    -DCPLEX_ROOT_DIR="${CPLEX_ROOT_DIR}"
 
 make -j${CPU_COUNT}
 make install
-
-if [ `uname` == Darwin ]; then
-	install_name_tool -change ${CPLEX_DIR}/cplex/lib/x86-64_osx/static_pic/libcplex.dylib @loader_path/./libcplex.dylib ${PREFIX}/lib/libpgmlink.dylib
-	install_name_tool -change ${CPLEX_DIR}/cplex/lib/x86-64_osx/static_pic/libilocplex.dylib @loader_path/./libilocplex.dylib ${PREFIX}/lib/libpgmlink.dylib
-	install_name_tool -change ${CPLEX_DIR}/concert/lib/x86-64_osx/static_pic/libconcert.dylib @loader_path/./libconcert.dylib ${PREFIX}/lib/libpgmlink.dylib
-	install_name_tool -change ${CPLEX_DIR}/cplex/lib/x86-64_osx/static_pic/libcplex.dylib @loader_path/./libcplex.dylib ${PREFIX}/lib/python2.7/site-packages/pgmlink.so
-	install_name_tool -change ${CPLEX_DIR}/cplex/lib/x86-64_osx/static_pic/libilocplex.dylib @loader_path/./libilocplex.dylib ${PREFIX}/lib/python2.7/site-packages/pgmlink.so
-	install_name_tool -change ${CPLEX_DIR}/concert/lib/x86-64_osx/static_pic/libconcert.dylib @loader_path/./libconcert.dylib ${PREFIX}/lib/python2.7/site-packages/pgmlink.so
-else
-    echo "Use chrpath here!"
-fi
---
+++ pgmlink/post-link.sh
@@ -0,0 +1,23 @@
+if [ -z "$CPLEX_ROOT_DIR" ]; then
+    echo "******************************************"
+    echo "* You must define CPLEX_ROOT_DIR in your *"
+    echo "* environment before installing pgmlink. *"
+    echo "******************************************"
+    exit 1
+fi
+
+if [ `uname` == Darwin ]; then
+    set -x
+    install_name_tool -change ${CPLEX_ROOT_DIR}/cplex/lib/x86-64_osx/static_pic/libcplex.dylib @loader_path/./libcplex.dylib ${PREFIX}/lib/libpgmlink.dylib
+    install_name_tool -change ${CPLEX_ROOT_DIR}/cplex/lib/x86-64_osx/static_pic/libilocplex.dylib @loader_path/./libilocplex.dylib ${PREFIX}/lib/libpgmlink.dylib
+    install_name_tool -change ${CPLEX_ROOT_DIR}/concert/lib/x86-64_osx/static_pic/libconcert.dylib @loader_path/./libconcert.dylib ${PREFIX}/lib/libpgmlink.dylib
+    install_name_tool -change ${CPLEX_ROOT_DIR}/cplex/lib/x86-64_osx/static_pic/libcplex.dylib @loader_path/./libcplex.dylib ${PREFIX}/lib/python2.7/site-packages/pgmlink.so
+    install_name_tool -change ${CPLEX_ROOT_DIR}/cplex/lib/x86-64_osx/static_pic/libilocplex.dylib @loader_path/./libilocplex.dylib ${PREFIX}/lib/python2.7/site-packages/pgmlink.so
+    install_name_tool -change ${CPLEX_ROOT_DIR}/concert/lib/x86-64_osx/static_pic/libconcert.dylib @loader_path/./libconcert.dylib ${PREFIX}/lib/python2.7/site-packages/pgmlink.so
+else
+    CPLEX_LIB_DIR=`echo $CPLEX_ROOT_DIR/cplex/lib/*/static_pic`
+    CONCERT_LIB_DIR=`echo $CPLEX_ROOT_DIR/concert/lib/*/static_pic`
+    set -x
+    patchelf --set-rpath $CONCERT_LIB_DIR:$CPLEX_LIB_DIR:'$ORIGIN/.' ${PREFIX}/lib/libpgmlink.so
+    patchelf --set-rpath $CONCERT_LIB_DIR:$CPLEX_LIB_DIR:'$ORIGIN/../..' ${PREFIX}/lib/python2.7/site-packages/pgmlink.so
+fi
