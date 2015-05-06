34837935bd5837933cbb6793481a0c3e68bba959
Stuart Berg <bergs@janelia.hhmi.org>
2015-05-06 14:33:55 -0400

    Added cplex-shared

---
+++ cplex-shared/post-link.sh
@@ -0,0 +1,74 @@
+CPLEX_LOCATION_CACHE_FILE="${PREFIX}/share/cplex-root-dir.path"
+
+if [ "$CPLEX_ROOT_DIR" == "" ]; then 
+    CPLEX_ROOT_DIR="<UNDEFINED>"
+fi
+
+if [ "$CPLEX_ROOT_DIR" != "<UNDEFINED>" ]; then
+    # If the environment-provided CPLEX_ROOT_DIR doesn't match the 
+    #  cache-provided value, (over)write the cache file.
+    SAVED_CPLEX_ROOT_DIR=`cat ${CPLEX_LOCATION_CACHE_FILE} 2> /dev/null` \
+    || SAVED_CPLEX_ROOT_DIR="<UNDEFINED>"
+    if [ "$SAVED_CPLEX_ROOT_DIR" != "$CPLEX_ROOT_DIR" ]; then
+	echo "${CPLEX_ROOT_DIR}" > ${CPLEX_LOCATION_CACHE_FILE}
+    fi
+fi 
+
+if [ "$CPLEX_ROOT_DIR" == "<UNDEFINED>" ]; then
+    # If we've installed at least once on this machine, 
+    # then we can get CPLEX_ROOT_DIR from our the cache file.
+    CPLEX_ROOT_DIR=`cat ${CPLEX_LOCATION_CACHE_FILE} 2> /dev/null` \
+    || CPLEX_ROOT_DIR="<UNDEFINED>"
+fi
+
+if [ "$CPLEX_ROOT_DIR" == "<UNDEFINED>" ]; then
+    set +x
+    echo "******************************************************"
+    echo "* You must define CPLEX_ROOT_DIR in your environment *"
+    echo "* before using cplex-shared for the first time.      *"
+    echo "******************************************************"
+    exit 1
+fi
+
+CPLEX_LIB_DIR=`echo $CPLEX_ROOT_DIR/cplex/lib/*/static_pic`
+CONCERT_LIB_DIR=`echo $CPLEX_ROOT_DIR/concert/lib/*/static_pic`
+
+# Check for write permissions in the CPLEX dir (we need to write to it).
+(
+    set -e
+    touch $CPLEX_LIB_DIR/testpermissions.empty.txt
+    rm $CPLEX_LIB_DIR/testpermissions.empty.txt
+    touch $CONCERT_LIB_DIR/testpermissions.empty.txt
+    rm $CONCERT_LIB_DIR/testpermissions.empty.txt
+)
+
+if [ $? -ne 0 ]; then
+    set +x
+    echo "************************************************************"
+    echo "* Could not create cplex-shared libraries.                 *"
+    echo "* CPLEX_ROOT_DIR is not writable.                          *"
+    echo "* Check the location and/or permissions of CPLEX_ROOT_DIR: *"
+    echo "* ${CPLEX_ROOT_DIR}                                        *"
+    echo "************************************************************"
+    exit 1
+fi
+
+# Create a shared library from each static library.
+if [ `uname` == "Darwin" ]; then
+    EXISTING_SHARED_OBJECT=`ls ${CPLEX_LIB_DIR}/libilocplex.dylib` \
+    || EXISTING_SHARED_OBJECT="NOT_PRESENT"
+    if [ "$EXISTING_SHARED_OBJECT" == "NOT_PRESENT" ]; then
+        g++ -fpic -shared -Wl,-all_load ${CPLEX_LIB_DIR}/libcplex.a     -Wl,-noall_load -o ${CPLEX_LIB_DIR}/libcplex.dylib
+        g++ -fpic -shared -Wl,-all_load ${CONCERT_LIB_DIR}/libconcert.a -Wl,-noall_load -o ${CONCERT_LIB_DIR}/libconcert.dylib
+        g++ -fpic -shared -Wl,-all_load ${CPLEX_LIB_DIR}/libilocplex.a  -Wl,-noall_load \
+            -L${CPLEX_LIB_DIR} -L${CONCERT_LIB_DIR} -lcplex -lconcert -o ${CPLEX_LIB_DIR}/libilocplex.dylib
+    fi
+else
+    EXISTING_SHARED_OBJECT=`ls ${CPLEX_LIB_DIR}/libilocplex.so` \
+    || EXISTING_SHARED_OBJECT="NOT_PRESENT"
+    if [ "$EXISTING_SHARED_OBJECT" == "NOT_PRESENT" ]; then
+        g++ -fpic -shared -Wl,-whole-archive ${CPLEX_LIB_DIR}/libcplex.a     -Wl,-no-whole-archive -o ${CPLEX_LIB_DIR}/libcplex.so
+	g++ -fpic -shared -Wl,-whole-archive ${CONCERT_LIB_DIR}/libconcert.a -Wl,-no-whole-archive -o ${CONCERT_LIB_DIR}/libconcert.so
+	g++ -fpic -shared -Wl,-whole-archive ${CPLEX_LIB_DIR}/libilocplex.a  -Wl,-no-whole-archive -o ${CPLEX_LIB_DIR}/libilocplex.so
+    fi
+fi
---
+++ cplex-shared/run_test.sh
@@ -0,0 +1,15 @@
+# This "test" just checks for the existence of the shared libraries craeted in the post-link step.
+CPLEX_LOCATION_CACHE_FILE="${PREFIX}/share/cplex-root-dir.path"
+CPLEX_ROOT_DIR=`cat ${CPLEX_LOCATION_CACHE_FILE}`
+CPLEX_LIB_DIR=`echo $CPLEX_ROOT_DIR/cplex/lib/*/static_pic`
+CONCERT_LIB_DIR=`echo $CPLEX_ROOT_DIR/concert/lib/*/static_pic`
+
+if [ `uname` == "Darwin" ]; then
+    ls ${CPLEX_LIB_DIR}/libcplex.dylib
+    ls ${CPLEX_LIB_DIR}/libilocplex.dylib
+    ls ${CONCERT_LIB_DIR}/libconcert.dylib
+else
+    ls ${CPLEX_LIB_DIR}/libcplex.so
+    ls ${CPLEX_LIB_DIR}/libilocplex.so
+    ls ${CONCERT_LIB_DIR}/libconcert.so
+fi
