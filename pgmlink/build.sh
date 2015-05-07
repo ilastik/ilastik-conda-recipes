4be964071bb81daf49c37d9c5b1736534a794eaf
Stuart Berg <bergs@janelia.hhmi.org>
2015-05-07 00:33:00 -0400

    pgmlink: Really use gcc for build.

--- pgmlink/build.sh
+++ pgmlink/build.sh
@@ -7,9 +7,15 @@ if [ "$CPLEX_ROOT_DIR" == "<UNDEFINED>" ]; then
     exit 1
 fi
 
+CFLAGS=""
+CXXFLAGS=""
+LDFLAGS=""
+
 mkdir build
 cd build
 cmake ..\
+    -DCMAKE_C_COMPILER=${PREFIX}/bin/gcc \
+    -DCMAKE_CXX_COMPILER=${PREFIX}/bin/g++ \
     -DCMAKE_OSX_DEPLOYMENT_TARGET=10.7\
     -DCMAKE_INSTALL_PREFIX=${PREFIX}\
     -DCMAKE_PREFIX_PATH=${PREFIX}\
