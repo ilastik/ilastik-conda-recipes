778a7353d5f87181357203c0540ddd78db61b45f
Stuart Berg <bergs@janelia.hhmi.org>
2015-05-08 15:51:22 -0400

    vtk: build with gcc on Linux

--- vtk/build.sh
+++ vtk/build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 if [ `uname` == Linux ]; then
-    CC=gcc44
-    CXX=g++44
-    CMAKE=cmake
+    CC=${PREFIX}/bin/gcc
+    CXX=${PREFIX}/bin/g++
     PY_LIB="libpython2.7.so"
 fi
+
+# Unfortunately, the VTK package can only be built with clang.
 if [ `uname` == Darwin ]; then
     CC=cc
     CXX=c++
@@ -16,16 +17,18 @@ fi
 
 mkdir build
 cd build
-$CMAKE \
+cmake \
+    -DCMAKE_C_COMPILER=${CC} \
+    -DCMAKE_CXX_COMPILER=${CXX} \
     -DCMAKE_INSTALL_PREFIX:PATH="$PREFIX" \
     -DCMAKE_INSTALL_RPATH:STRING="$PREFIX/lib" \
     -DVTK_HAS_FEENABLEEXCEPT:BOOL=OFF \
     -DBUILD_TESTING:BOOL=OFF \
     -DBUILD_EXAMPLES:BOOL=OFF \
     -DBUILD_SHARED_LIBS:BOOL=ON \
-    -DPYTHON_EXECUTABLE:FILEPATH=$PYTHON \
-    -DPYTHON_INCLUDE_PATH:PATH=$PREFIX/include/python2.7 \
-    -DPYTHON_LIBRARY:FILEPATH=$PREFIX/lib/$PY_LIB \
+    -DPYTHON_EXECUTABLE:FILEPATH=${PYTHON} \
+    -DPYTHON_INCLUDE_PATH:PATH=${PREFIX}/include/python2.7 \
+    -DPYTHON_LIBRARY:FILEPATH=${PREFIX}/lib/${PY_LIB} \
     -DVTK_USE_X:BOOL=OFF \
     -DVTK_WRAP_PYTHON:BOOL=ON \
     -DVTK_USE_OFFSCREEN:BOOL=ON \
