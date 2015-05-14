2a850d8ee093f59dcd7aa6f07933244f441c41b6
Stuart Berg <bergs@janelia.hhmi.org>
2015-05-14 15:54:11 -0400

    Added iiboost

---
+++ iiboost/build.sh
@@ -0,0 +1,24 @@
+# Get commonly needed env vars
+CWD=$(cd `dirname $0` && pwd)
+source $CWD/../common-vars.sh
+
+IIBOOST_LDFLAGS="${CXX_LDFLAGS} -Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib"
+
+mkdir build
+cd build
+cmake .. \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DCMAKE_PREFIX_PATH=${PREFIX} \
+    -DCMAKE_SHARED_LINKER_FLAGS="${IIBOOST_LDFLAGS}" \
+    -DCMAKE_EXE_LINKER_FLAGS="${IIBOOST_LDFLAGS}" \
+    -DBUILD_PYTHON_WRAPPER=1 \
+    -DPYTHON_BASE_PATH=${PREFIX} \
+    -DITK_DIR=${PREFIX}/lib/cmake/ITK-4.6 \
+##
+
+# BUILD
+make -j${CPU_COUNT}
+
+# "install" to the build prefix (conda will relocate these files afterwards)
+make install
