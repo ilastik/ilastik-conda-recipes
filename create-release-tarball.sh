2fa880410a56847877e0eca49e18cc4440460242
Stuart Berg <bergs@janelia.hhmi.org>
2015-05-15 16:03:46 -0400

    create-release-tarball: Fixed bug in args to 'conda remove --all'

--- create-release-tarball.sh
+++ create-release-tarball.sh
@@ -3,7 +3,7 @@ set -e
 CONDA_ROOT=`conda info --root`
 
 # Remove old ilastik-release environment
-conda remove -q -y --all ilastik-release 2> /dev/null || true
+conda remove -q -y --all -n ilastik-release 2> /dev/null || true
 
 # Create new ilastik-release environment and install all ilastik dependencies to it.
 conda create -y -n ilastik-release ilastik-deps-pc ilastik-deps-carving ilastik-deps-tracking ilastik-meta
