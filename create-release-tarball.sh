b3b5ae98d20bef9c7df776b134612d7accf8f805
Stuart Berg <bergs@janelia.hhmi.org>
2015-05-15 16:31:44 -0400

    create-release-tarball.sh: Tarball name includes release version.

--- create-release-tarball.sh
+++ create-release-tarball.sh
@@ -8,8 +8,10 @@ conda remove -q -y --all -n ilastik-release 2> /dev/null || true
 # Create new ilastik-release environment and install all ilastik dependencies to it.
 conda create -y -n ilastik-release ilastik-deps-pc ilastik-deps-carving ilastik-deps-tracking ilastik-meta
 
+ILASTIK_PKG_VERSION=`conda list -n ilastik-release | grep ilastik-meta | python -c "import sys; print sys.stdin.read().split()[1]"`
+RELEASE_NAME=ilastik-${ILASTIK_PKG_VERSION}-Linux
+
 # Create the tarball, and move it to the current directory.
-pushd ${CONDA_ROOT}/envs
-tar czf ilastik-release.tar.gz ilastik-release
-popd
-mv ${CONDA_ROOT}/envs/ilastik-release.tar.gz .
+tar czf ${RELEASE_NAME}.tar.gz \
+    --transform "s|${CONDA_ROOT:1}/envs/ilastik-release|${RELEASE_NAME}|" \
+    ${CONDA_ROOT}/envs/ilastik-release
