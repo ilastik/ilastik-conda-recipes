c10e8e7f56a6dedb7a296b7854adc9589d12f0ef
Stuart Berg <bergs@janelia.hhmi.org>
2015-05-18 19:27:57 -0400

    ilastik-everything: Stop lazyflow test after first failure.

--- ilastik-everything/run_test.sh
+++ ilastik-everything/run_test.sh
@@ -7,7 +7,7 @@ cd ${PREFIX}/ilastik-meta/volumina/tests
 nosetests .
 
 cd ${PREFIX}/ilastik-meta/lazyflow/tests
-nosetests --ignore-files testInterpolatedFeatures.py .
+nosetests --stop --ignore-files testInterpolatedFeatures.py .
 
 cd ${PREFIX}/ilastik-meta/ilastik/tests
 ./run_each_unit_test.sh
