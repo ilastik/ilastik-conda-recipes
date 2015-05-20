87136842284558b14f111cfe4c24e12bd90d4859
Stuart Berg <bergs@janelia.hhmi.org>
2015-05-20 08:46:07 -0400

    jsoncpp, libdvid-cpp: Use gcc package, also set rpath

--- jsoncpp/build.sh
+++ jsoncpp/build.sh
@@ -6,12 +6,14 @@ source $CWD/../common-vars.sh
 mkdir build
 cd build
 cmake ..\
+        -DCMAKE_C_COMPILER=${PREFIX}/bin/gcc \
+        -DCMAKE_CXX_COMPILER=${PREFIX}/bin/g++ \
         -DCMAKE_INSTALL_PREFIX=${PREFIX} \
         -DCMAKE_PREFIX_PATH=${PREFIX} \
-        -DCMAKE_CXX_FLAGS="-fPIC"
+        -DCMAKE_CXX_FLAGS="-fPIC" \
         -DBUILD_SHARED_LIBS=1 \
-        -DCMAKE_SHARED_LINKER_FLAGS=-L${PREFIX}/lib \
-        -DCMAKE_EXE_LINKER_FLAGS=-L${PREFIX}/lib \
+        -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
+        -DCMAKE_EXE_LINKER_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
         -DPYTHON_EXECUTABLE=${PYTHON} \
 
 # BUILD
--- libdvid-cpp/build.sh
+++ libdvid-cpp/build.sh
@@ -6,11 +6,13 @@ source $CWD/../common-vars.sh
 mkdir build
 cd build
 cmake ..\
+        -DCMAKE_C_COMPILER=${PREFIX}/bin/gcc \
+        -DCMAKE_CXX_COMPILER=${PREFIX}/bin/g++ \
         -DCMAKE_INSTALL_PREFIX=${PREFIX} \
         -DCMAKE_PREFIX_PATH=${PREFIX} \
         -DCMAKE_CXX_FLAGS=-I${PREFIX}/include \
-        -DCMAKE_SHARED_LINKER_FLAGS=-L${PREFIX}/lib \
-        -DCMAKE_EXE_LINKER_FLAGS=-L${PREFIX}/lib \
+        -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
+        -DCMAKE_EXE_LINKER_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
         -DBoost_INCLUDE_DIR=${PREFIX}/include \
         -DPYTHON_EXECUTABLE=${PYTHON} \
         -DLIBDVID_WRAP_PYTHON=1 \
