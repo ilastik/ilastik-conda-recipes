3e28c75276fb785442580a8f765f2b4e5425b0c7
Carsten Haubold <carstenhaubold@googlemail.com>
2015-05-21 20:55:31 +0200

    Add pgmlink build with gurobi instead of CPLEX

---
+++ pgmlink-gurobi/post-link.sh
@@ -0,0 +1,18 @@
+if [ ! -d $GUROBI_ROOT_DIR ]
+then
+    echo "Error: GUROBI_ROOT_DIR needs to be set"
+    exit 1
+fi
+
+if [ `uname` == Darwin ]; then
+    set -x
+    install_name_tool -add_rpath ${GUROBI_ROOT_DIR}/lib "${PREFIX}/lib/libpgmlink.dylib"
+    
+    install_name_tool -add_rpath ${GUROBI_ROOT_DIR}/lib  "${PREFIX}/lib/python2.7/site-packages/pgmlink.so"
+    set +x
+else
+    set -x
+    patchelf --set-rpath ${GUROBI_ROOT_DIR}/lib:'$ORIGIN/.' ${PREFIX}/lib/libpgmlink.so
+    patchelf --set-rpath ${GUROBI_ROOT_DIR}/lib:'$ORIGIN/../..' ${PREFIX}/lib/python2.7/site-packages/pgmlink.so
+    set +x
+fi
