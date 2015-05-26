0004179ba91009f82dd8e6adc056bd0378fa7166
Stuart Berg <bergs@janelia.hhmi.org>
2015-05-26 17:16:45 -0400

    create-linux-tarball.sh: Added --git-head option.

--- create-linux-tarball.sh
+++ create-linux-tarball.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
 ##
-## Usage: create-linux-tarball.sh [--use-local] [-c binstar_channel]
+## Usage: create-linux-tarball.sh [--git-head] [--use-local] [-c binstar_channel]
 ##
 
 set -e
+
+USE_GIT_HEAD=0
+if [[ $@ == *"--git-head"* ]]; then
+    if [[ $1 == "--git-head" ]]; then
+	USE_GIT_HEAD=1
+	shift
+    else
+	echo "Error: --git-head may only be provided as the first arg." >&2
+	exit 1
+    fi
+fi
+
 CONDA_ROOT=`conda info --root`
 source ${CONDA_ROOT}/bin/activate root
 
@@ -18,9 +30,22 @@ fi
 echo "Creating new ilastik-release environment..."
 conda create -q -y -n ilastik-release ilastik-everything $1 $2 $3
 
-# Ask conda for the package version
-ILASTIK_PKG_VERSION=`conda list -n ilastik-release | grep ilastik-meta | python -c "import sys; print sys.stdin.read().split()[1]"`
-RELEASE_NAME=ilastik-${ILASTIK_PKG_VERSION}-Linux
+if [[ $USE_GIT_HEAD == 1 ]]; then
+    # Instead of keeping the version from binstar, get the git repo
+    ILASTIK_META=${CONDA_ROOT}/envs/ilastik-release/ilastik-meta
+    rm -rf ${ILASTIK_META}
+    git clone https://github.com/ilastik/ilastik-meta ${ILASTIK_META}
+    cd ${ILASTIK_META}
+    git submodule init
+    git submodule update
+    git submodule foreach 'git checkout master'
+    cd -
+    ILASTIK_PKG_VERSION="master"
+else
+    # Ask conda for the package version
+    ILASTIK_PKG_VERSION=`conda list -n ilastik-release | grep ilastik-meta | python -c "import sys; print sys.stdin.read().split()[1]"`
+fi
+RELEASE_NAME=ilastik-${ILASTIK_PKG_VERSION}-`uname`
 
 # Create the tarball, and move it to the current directory.
 echo "Creating ${RELEASE_NAME}.tar.gz"
