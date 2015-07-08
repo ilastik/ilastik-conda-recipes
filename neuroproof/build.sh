e62217ce67e43ead9e31d1416a93375075f4075b
Stephen Plaza <plazas@janelia.hhmi.org>
2015-07-07 21:26:04 -0400

    adds neuroproof

---
+++ neuroproof/build.sh
@@ -0,0 +1,32 @@
+# Get commonly needed env vars
+CWD=$(cd `dirname $0` && pwd)
+source $CWD/../common-vars.sh
+
+# CONFIGURE
+mkdir build
+cd build
+cmake ..\
+        -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+        -DCMAKE_PREFIX_PATH=${PREFIX} \
+        -DCMAKE_CXX_FLAGS=-I${PREFIX}/include \
+        -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
+        -DCMAKE_EXE_LINKER_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
+        -DBoost_INCLUDE_DIR=${PREFIX}/include \
+        -DPYTHON_EXECUTABLE=${PYTHON} \
+        -DPYTHON_LIBRARY=${PREFIX}/lib/libpython2.7.${DYLIB_EXT} \
+        -DPYTHON_INCLUDE_DIR=${PREFIX}/include/python2.7 \
+
+# BUILD
+make -j${CPU_COUNT}
+
+# TEST (before install)
+(
+    # (Since conda hasn't performed its link step yet, we must 
+    #  help the tests locate their dependencies via LD_LIBRARY_PATH)
+    export ${LIBRARY_SEARCH_VAR}=$PREFIX/lib:${!LIBRARY_SEARCH_VAR}
+    export PYTHONPATH=${SRC_DIR}/lib
+    make test
+)
+
+# "install" to the build prefix (conda will relocate these files afterwards)
+make install
