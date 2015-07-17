9f384867745ad5e2039a596ac2611297a7209046
Stuart Berg <bergs@janelia.hhmi.org>
2015-07-17 14:30:44 -0400

    Added neutu recipe

---
+++ neutu/build.sh
@@ -0,0 +1,16 @@
+if [ `uname` == 'Darwin' ]; then
+    # Unfortunately, this package must be built with clang, not conda's gcc
+    CC=/usr/bin/cc
+    CXX=/usr/bin/c++
+fi
+
+if [ `uname` == 'Darwin' ]; then
+    QMAKE_SPEC_PATH=${PREFIX}/mkspecs/macx-g++
+else
+    QMAKE_SPEC_PATH=${PREFIX}/mkspecs/linux-g++-64
+fi
+
+bash build.sh ${PREFIX}/bin/qmake ${QMAKE_SPEC_PATH} -e flyem
+
+# Install to conda environment
+mv neurolabi/build/neuTube.app ${PREFIX}/bin/
