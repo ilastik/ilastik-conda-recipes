ae1c350ac2701f628174349dd6cb2dd1aa0be1c6
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2015-08-20 16:12:38 +0200

    Python: install according to conda conventions, add visual_studio dependency

--- python/bld.bat
+++ python/bld.bat
@@ -33,14 +33,15 @@ devenv PCbuild.sln /build "Release|%PYTHON_BITNESS%" /project select
 if errorlevel 1 exit 1
 devenv PCbuild.sln /build "Release|%PYTHON_BITNESS%" /project unicodedata
 if errorlevel 1 exit 1
-devenv PCbuild.sln /build "Release|%PYTHON_BITNESS%" /project bz2
-if errorlevel 1 exit 1
+REM bz2 source is not included in the Python tar ball, skip it for now
+REM FIXME: provide a recipe for bz2
+REM devenv PCbuild.sln /build "Release|%PYTHON_BITNESS%" /project bz2
+REM if errorlevel 1 exit 1
 
 REM patch distutils for Visual Studio 2012
 "%PYTHON_EXE%" "%RECIPE_DIR%/patch_python.py" ../Lib/distutils/msvc9compiler.py ../Lib/distutils/cygwinccompiler.py
 if errorlevel 1 exit 1
 
-REM install 
-REM FIXME: what's the correct install path?
+REM install directly into %PREFIX% (Conda convention on Windows)
 cmake -DPYTHON_BIN="%PYTHON_BIN%" -DPYTHON_INSTALL_PREFIX="%PREFIX%" -P "%RECIPE_DIR%/python_install.cmake" 
 if errorlevel 1 exit 1
