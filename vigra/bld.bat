6af179a82acbdb16b64c1dca4f2cb57a815856d8
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2015-08-24 16:14:58 +0200

    added Windows build to vigra recipe

--- vigra/bld.bat
+++ vigra/bld.bat
@@ -4,6 +4,7 @@ mkdir build
 cd build
 
 set CONFIGURATION=Release
+set PATH=%PATH%;%LIBRARY_PREFIX%\bin
 
 cmake .. -G "%CMAKE_GENERATOR%" -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%
 if errorlevel 1 exit 1
@@ -14,6 +15,12 @@ if errorlevel 1 exit 1
 
 REM TEST
 cmake --build . --target test_impex --config %CONFIGURATION%
+if errorlevel 1 exit 1
+cmake --build . --target test_hdf5impex --config %CONFIGURATION%
+if errorlevel 1 exit 1
+cmake --build . --target test_fourier --config %CONFIGURATION%
+if errorlevel 1 exit 1
+cmake --build . --target vigranumpytest --config %CONFIGURATION%
 if errorlevel 1 exit 1
 
 REM INSTALL
