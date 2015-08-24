1a5f0a36e995dacc3ee75462f6d2263b3df340b7
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2015-08-24 16:14:24 +0200

    added numpy recipe

--- numpy/bld.bat
+++ numpy/bld.bat
@@ -3,13 +3,8 @@ call "%RECIPE_DIR%\..\common-vars.bat"
 patch -p0 -i "%RECIPE_DIR%\patch_numpy.patch"
 if errorlevel 1 exit 1
 
-rem the install path must have forward slashes
-set DEPENDENCY_PREFIX=%LIBRARY_PREFIX%
-setlocal enabledelayedexpansion
-FOR %%f IN (%DEPENDENCY_PREFIX%) DO (
-  set "f=%%f"
-  set "DEPENDENCY_PREFIX=!f:\=/!"
-)
+rem the dependency path must have forward slashes
+%DOS_TOOLS% :to_linux_path %LIBRARY_PREFIX% DEPENDENCY_PREFIX
 cat "%RECIPE_DIR%/site.cfg.in" | sed -e "s@${DEPENDENCY_PREFIX}@%DEPENDENCY_PREFIX%@g" > site.cfg
 if errorlevel 1 exit 1
 
