1751ca10180d3e64df6d18e2b8210909bea4681c
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2015-08-25 20:22:56 +0200

    dos-tools to_linux_path: remove quotes from input path

--- dos-tools.bat
+++ dos-tools.bat
@@ -6,13 +6,13 @@ goto :eof
 REM convert backward slashes to forward slashes
 REM   :to_linux_path path OUT_VAR
 :to_linux_path
-    set _TMP_PATH=%1
+    set _TMP_PATH=%~1
     call set "%~2=%_TMP_PATH:\=/%"
     goto :EOF
     
 REM convert forward slashes to backward slashes
 REM   :to_dos_path path OUT_VAR
 :to_dos_path
-    set _TMP_PATH=%1
+    set _TMP_PATH=%~1
     call set "%~2=%_TMP_PATH:/=\%"
     goto :EOF
