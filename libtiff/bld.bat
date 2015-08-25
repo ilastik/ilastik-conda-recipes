61b0b5a5ba6db43b7251df3d3095b6f036cdc47c
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2015-08-25 20:25:18 +0200

    libtiff: fixed configuration

--- libtiff/bld.bat
+++ libtiff/bld.bat
@@ -1,14 +1,18 @@
 call "%RECIPE_DIR%\..\common-vars.bat"
 
+%DOS_TOOLS% :to_linux_path "%LIBRARY_PREFIX%\include" INCLUDE_PATH
+%DOS_TOOLS% :to_linux_path "%LIBRARY_PREFIX%\lib" LIB_PATH
+
 rem patch nmake.opt
-echo # >> nmake.opt
-echo JPEG_SUPPORT = 1 >> nmake.opt
-echo JPEG_INCLUDE = -I%LIBRARY_PREFIX%/include >> nmake.opt
-echo JPEG_LIB = -I%LIBRARY_PREFIX%/lib/jpeg.lib >> nmake.opt
-echo # >> nmake.opt
-echo ZIP_SUPPORT = 1 >> nmake.opt
-echo ZLIB_INCLUDE = -I%LIBRARY_PREFIX%/include >> nmake.opt
-echo ZLIB_LIB = -I%LIBRARY_PREFIX%/lib/zlib.lib >> nmake.opt
+cat nmake.opt ^
+  | sed -e "s@#JPEG_SUPPORT@JPEG_SUPPORT@g" ^
+        -e "s@#JPEG_INCLUDE.*@JPEG_INCLUDE = -I\"%INCLUDE_PATH%\"@g" ^
+        -e "s@#JPEG_LIB.*@JPEG_LIB = \"%LIB_PATH%/jpeg.lib\"@g" ^
+        -e "s@#ZIP_SUPPORT@ZIP_SUPPORT@g" ^
+        -e "s@#ZLIB_INCLUDE.*@ZLIB_INCLUDE = -I\"%INCLUDE_PATH%\"@g" ^
+        -e "s@#ZLIB_LIB.*@ZLIB_LIB = \"%LIB_PATH%/zlib.lib\"@g" ^
+  > nmake.opt.new
+move nmake.opt.new nmake.opt
 
 REM BUILD
 nmake /f Makefile.vc
