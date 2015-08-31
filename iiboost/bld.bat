a73a9a4a08660a266612531b20e91c0d967a189e
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2015-08-31 16:19:26 +0200

    added windows build to iiboost recipe

---
+++ iiboost/bld.bat
@@ -0,0 +1,17 @@
+call "%RECIPE_DIR%\..\common-vars.bat"
+
+mkdir build
+cd build
+
+%DOS_TOOLS% :to_linux_path "%LIBRARY_PREFIX%" LIBRARY_PREFIX_LINUX
+set ITK_CONFIG_DIR=%LIBRARY_PREFIX_LINUX%/lib/cmake/ITK-4.6
+
+cmake .. -G "%CMAKE_GENERATOR%" ^
+         -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX_LINUX%" ^
+         -DCMAKE_BUILD_TYPE=Release ^
+         -DBUILD_PYTHON_WRAPPER=1 ^
+         -DITK_DIR="%ITK_CONFIG_DIR%"
+if errorlevel 1 exit 1
+            
+cmake --build . --target INSTALL --config Release
+if errorlevel 1 exit 1
