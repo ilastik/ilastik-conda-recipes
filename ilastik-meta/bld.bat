1e7498c4d54aad1def6d36c3905e3bc1e2e81b97
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2015-08-31 20:36:43 +0200

    ilastik-meta: added Windows build

---
+++ ilastik-meta/bld.bat
@@ -0,0 +1,59 @@
+call "%RECIPE_DIR%\..\common-vars.bat"
+
+REM The git hash is prefixed with 'g' for some stupid reason.
+git checkout %GIT_DESCRIBE_HASH:~1% 
+if errorlevel 1 exit 1
+git submodule update --recursive
+if errorlevel 1 exit 1
+
+if not exist tmp mkdir tmp
+copy "%RECIPE_DIR%\read-version.py" tmp
+
+REM Verify that the git tag and the python version match (a common error).
+%DOS_TOOLS% :capture_output "python tmp\read-version.py ilastik/ilastik/__init__.py" ILASTIK_CODE_VERSION
+
+if not "%ILASTIK_CODE_VERSION%"=="%PKG_VERSION%" ( 
+    echo ********************************************************************************
+    echo ilastik-meta package version does not match __version_info__ in ilastik/__init__.py
+    echo ilastik-meta version:      %PKG_VERSION%
+    echo ilastik.__version_info__:  %ILASTIK_CODE_VERSION%
+    echo ********************************************************************************
+    exit 1
+)
+
+REM Create .pyc files
+python -m compileall lazyflow volumina ilastik
+if errorlevel 1 exit 1
+
+REM Add the ilastik modules to sys.path
+copy "%RECIPE_DIR%\ilastik-meta.pth.win" "%PREFIX%\Lib\site-packages\ilastik-meta.pth"
+if errorlevel 1 exit 1
+
+REM Install the ilastik modules
+xcopy /S lazyflow "%PREFIX%\ilastik-meta\lazyflow\"
+if errorlevel 1 exit 1
+xcopy /S volumina "%PREFIX%\ilastik-meta\volumina\"
+if errorlevel 1 exit 1
+xcopy /S ilastik "%PREFIX%\ilastik-meta\ilastik\"
+if errorlevel 1 exit 1
+copy Readme* "%PREFIX%\ilastik-meta\"
+if errorlevel 1 exit 1
+copy "%RECIPE_DIR%\run-ilastik.bat" "%PREFIX%\"
+if errorlevel 1 exit 1
+
+REM build ilastik.exe
+mkdir build
+cd build
+
+cmake "%RECIPE_DIR%\ilastik_launch" ^
+        -G "%CMAKE_GENERATOR%" ^
+        -DCMAKE_INSTALL_PREFIX="%LIBRARY_BIN%"
+if errorlevel 1 exit 1
+
+cmake --build . --target ilastik --config Release
+if errorlevel 1 exit 1
+
+cmake --build . --target INSTALL --config Release
+if errorlevel 1 exit 1
+
+cd ..
