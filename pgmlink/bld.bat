2073cd2239ae7e7272066a6ef2572e69880ba579
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2015-08-31 12:12:53 +0200

    added windows build to pgmlink recipe

---
+++ pgmlink/bld.bat
@@ -0,0 +1,26 @@
+call "%RECIPE_DIR%\..\common-vars.bat"
+
+mkdir build
+cd build
+
+%DOS_TOOLS% :to_linux_path "%LIBRARY_PREFIX%" LIBRARY_PREFIX_LINUX
+
+cmake .. -G "%CMAKE_GENERATOR%" ^
+         -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX_LINUX%" ^
+         -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX_LINUX%" ^
+         -DCMAKE_BUILD_TYPE=Release ^
+         -DWITH_CHECKED_STL=OFF ^
+         -DWITH_PYTHON=ON ^
+         -DWITH_TESTS=OFF ^
+         -DPYTHON_INCLUDE_DIRS="%PREFIX%\include" ^
+         -DPYTHON_LIBRARIES="%PREFIX%\libs\python27.lib"
+if errorlevel 1 exit 1
+    
+set CONFIGURATION=Release
+cmake --build . --target pgmlink --config %CONFIGURATION%
+if errorlevel 1 exit 1
+cmake --build . --target pypgmlink --config %CONFIGURATION%
+if errorlevel 1 exit 1
+
+cmake --build . --target INSTALL --config %CONFIGURATION%
+if errorlevel 1 exit 1
