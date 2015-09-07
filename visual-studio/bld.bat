e56cbc862ee82fc4d890e58d1962f2ed785ffaa8
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2015-09-07 22:47:40 +0200

    visual studio: configure toolset-info

--- visual-studio/bld.bat
+++ visual-studio/bld.bat
@@ -1,16 +1,26 @@
-@echo off
+@echo on
 
-call "%RECIPE_DIR%\..\common-vars.bat"
+REM create toolset info
+set TOOLSET_INFO_DIR=%PREFIX%\toolset-info
+if not exist "%TOOLSET_INFO_DIR%" mkdir "%TOOLSET_INFO_DIR%"
 
-REM check if we have the correct compiler version
-if NOT "%VISUAL_STUDIO_VERSION%"=="%PKG_VERSION%" (
-    echo Visual Studio %PKG_VERSION% required.
-    exit 1
-)
+cat "%RECIPE_DIR%\common-vars.bat.in" ^
+   | sed -e "s/@PKG_VERSION@/%PKG_VERSION%/g" ^
+   > "%TOOLSET_INFO_DIR%\common-vars.bat"
+   
+cat "%RECIPE_DIR%\config.yaml.in" ^
+   | sed -e "s/@PKG_VERSION@/%PKG_VERSION%/g" ^
+         -e "s/@PKG_SHORT@/vc%PKG_VERSION:.0=%/g" ^
+   > "%TOOLSET_INFO_DIR%\config.yaml"
+   
+copy "%RECIPE_DIR%\dos-tools.bat" "%TOOLSET_INFO_DIR%\"
+   
+call "%TOOLSET_INFO_DIR%\common-vars.bat"
 
 REM install Visual Studio runtime libraries (msvcr, msvcp)
 cmake "%RECIPE_DIR%" -G "%CMAKE_GENERATOR%" -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"
 if errorlevel 1 exit 1
 cmake --build . --target INSTALL --config Release
-move "%LIBRARY_PREFIX%\bin\msvc*.dll" "%PREFIX%"
+if errorlevel 1 exit 1
+copy "%LIBRARY_PREFIX%\bin\msvc*.dll" "%PREFIX%"
 if errorlevel 1 exit 1
