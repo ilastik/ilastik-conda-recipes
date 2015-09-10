cd4965193c7e9f489f0807fa44abab8ba24fe73b
Stuart Berg <bergs@janelia.hhmi.org>
2015-09-09 22:24:02 -0400

    neuroproof: build.sh: Hygenic quoting fixes.

--- neuroproof/build.sh
+++ neuroproof/build.sh
@@ -9,19 +9,19 @@ fi
 mkdir -p build # Using -p here is convenient for calling this script outside of conda.
 cd build
 cmake ..\
-        -DCMAKE_C_COMPILER=${PREFIX}/bin/gcc \
-        -DCMAKE_CXX_COMPILER=${PREFIX}/bin/g++ \
-        -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-        -DCMAKE_PREFIX_PATH=${PREFIX} \
-        -DCMAKE_CXX_FLAGS=-I${PREFIX}/include \
+        -DCMAKE_C_COMPILER="${PREFIX}/bin/gcc" \
+        -DCMAKE_CXX_COMPILER="${PREFIX}/bin/g++" \
+        -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+        -DCMAKE_PREFIX_PATH="${PREFIX}" \
+        -DCMAKE_CXX_FLAGS=-I"${PREFIX}/include" \
         -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
         -DCMAKE_EXE_LINKER_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
-        -DBoost_INCLUDE_DIR=${PREFIX}/include \
+        -DBoost_INCLUDE_DIR="${PREFIX}/include" \
         -DENABLE_GUI=1 \
-        -DPYTHON_EXECUTABLE=${PYTHON} \
-        -DPYTHON_LIBRARY=${PREFIX}/lib/libpython2.7.${DYLIB_EXT} \
-        -DPYTHON_INCLUDE_DIR=${PREFIX}/include/python2.7 \
-
+        -DPYTHON_EXECUTABLE="${PYTHON}" \
+        -DPYTHON_LIBRARY="${PREFIX}/lib/libpython2.7.${DYLIB_EXT}" \
+        -DPYTHON_INCLUDE_DIR="${PREFIX}/include/python2.7" \
+##
 # BUILD
 make -j${CPU_COUNT}
 
