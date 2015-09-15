af004b9003d5b52994a284930978a62fd13c9052
Stuart Berg <bergs@janelia.hhmi.org>
2015-09-15 19:03:51 -0400

    libdvid-cpp: Enable usage from outside conda: - Don't rely on common-vars.sh - Use mkdir -p - Added --configure-only option - Moved test step into run_test.sh

--- libdvid-cpp/build.sh
+++ libdvid-cpp/build.sh
@@ -1,36 +1,43 @@
-# Get commonly needed env vars
-CWD=$(cd `dirname $0` && pwd)
-source $CWD/../common-vars.sh
+# Depending on our platform, shared libraries end with either .so or .dylib
+if [[ `uname` == 'Darwin' ]]; then
+    DYLIB_EXT=dylib
+else
+    DYLIB_EXT=so
+fi
+
+CONFIGURE_ONLY=0
+if [[ $1 != "" ]]; then
+    if [[ $1 == "--configure-only" ]]; then
+        CONFIGURE_ONLY=1
+    else
+        echo "Unknown argument: $1"
+        exit 1
+    fi
+fi
 
 # CONFIGURE
-mkdir build
+mkdir -p build # Using -p here is convenient for calling this script outside of conda.
 cd build
 cmake ..\
-        -DCMAKE_C_COMPILER=${PREFIX}/bin/gcc \
-        -DCMAKE_CXX_COMPILER=${PREFIX}/bin/g++ \
-        -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-        -DCMAKE_PREFIX_PATH=${PREFIX} \
-        -DCMAKE_CXX_FLAGS=-I${PREFIX}/include \
+        -DCMAKE_C_COMPILER="${PREFIX}/bin/gcc" \
+        -DCMAKE_CXX_COMPILER="${PREFIX}/bin/g++" \
+        -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+        -DCMAKE_PREFIX_PATH="${PREFIX}" \
+        -DCMAKE_CXX_FLAGS=-I"${PREFIX}/include" \
         -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
         -DCMAKE_EXE_LINKER_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
-        -DBoost_LIBRARY_DIR=${PREFIX}/lib \
-        -DBoost_INCLUDE_DIR=${PREFIX}/include \
-        -DPYTHON_EXECUTABLE=${PYTHON} \
-        -DPYTHON_LIBRARY=${PREFIX}/lib/libpython2.7.${DYLIB_EXT} \
-        -DPYTHON_INCLUDE_DIR=${PREFIX}/include/python2.7 \
+        -DBoost_LIBRARY_DIR="${PREFIX}/lib" \
+        -DBoost_INCLUDE_DIR="${PREFIX}/include" \
+        -DPYTHON_EXECUTABLE="${PYTHON}" \
+        -DPYTHON_LIBRARY="${PREFIX}/lib/libpython2.7.${DYLIB_EXT}" \
+        -DPYTHON_INCLUDE_DIR="${PREFIX}/include/python2.7" \
         -DLIBDVID_WRAP_PYTHON=1 \
+##
 
-# BUILD
-make -j${CPU_COUNT}
+if [[ $CONFIGURE_ONLY == 0 ]]; then
+    # BUILD
+    make -j${CPU_COUNT}
 
-# TEST (before install)
-(
-    # (Since conda hasn't performed its link step yet, we must 
-    #  help the tests locate their dependencies via LD_LIBRARY_PATH)
-    export ${LIBRARY_SEARCH_VAR}=$PREFIX/lib:${!LIBRARY_SEARCH_VAR}
-    export PYTHONPATH=${SRC_DIR}/python
-    make test
-)
-
-# "install" to the build prefix (conda will relocate these files afterwards)
-make install
+    # "install" to the build prefix (conda will relocate these files afterwards)
+    make install
+fi
