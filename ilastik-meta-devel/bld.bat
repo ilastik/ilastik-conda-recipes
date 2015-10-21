739aee6dfc4cd2922e4b56f069fcad18636672a6
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2015-10-21 20:45:52 +0200

    ilastik-meta-devel: don't remove ilastik-meta when it contains uncommited/unpushed changes

--- ilastik-meta-devel/bld.bat
+++ ilastik-meta-devel/bld.bat
@@ -3,4 +3,9 @@ if not exist "%PREFIX%\Lib\site-packages" mkdir "%PREFIX%\Lib\site-packages"
 copy "%RECIPE_DIR%\ilastik-meta.pth.win" "%PREFIX%\Lib\site-packages\ilastik-meta.pth"
 if errorlevel 1 exit 1
 
+REM create pre-unlink.bat
+type "%RECIPE_DIR%\..\check_and_delete_git_repo.bat.in" ^
+   | sed -e "s$@REPO_REL_PATH@$ilastik-meta$g" ^
+   > "%RECIPE_DIR%\pre-unlink.bat"
+
 REM Checkout will be done in post-link.bat
