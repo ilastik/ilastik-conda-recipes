a569d07a954ab9778cc013e6323e4965cd984e0b
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2015-10-26 17:34:18 +0100

    added eigen and vc-simd recipes

---
+++ eigen/bld.bat
@@ -0,0 +1,1 @@
+xcopy /S /Q Eigen "%LIBRARY_INC%\"
---
+++ vc-simd/bld.bat
@@ -0,0 +1,17 @@
+REM load toolset info
+set TOOLSET_INFO_DIR=%PREFIX%\toolset-info
+call "%TOOLSET_INFO_DIR%\common-vars.bat"
+
+mkdir build
+cd build
+
+cmake .. -G "%CMAKE_GENERATOR%" -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" -DBUILD_TESTING=OFF
+if errorlevel 1 exit 1
+
+REM BUILD
+cmake --build . --target ALL_BUILD --config Release
+if errorlevel 1 exit 1
+
+REM INSTALL
+cmake --build . --target INSTALL --config Release
+if errorlevel 1 exit 1
