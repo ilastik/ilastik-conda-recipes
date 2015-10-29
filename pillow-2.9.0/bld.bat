f6491f58a44243ed79b05700bbecdb8b271d7977
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2015-10-29 11:49:40 +0100

    upgraded pillow to version 3.0.0

---
+++ pillow-2.9.0/bld.bat
@@ -0,0 +1,21 @@
+REM load toolset info
+set TOOLSET_INFO_DIR=%PREFIX%\toolset-info
+call "%TOOLSET_INFO_DIR%\common-vars.bat"
+
+%DOS_TOOLS% :to_linux_path "%LIBRARY_PREFIX%\include" INCLUDE_PATH
+%DOS_TOOLS% :to_linux_path "%LIBRARY_PREFIX%\lib" LIB_PATH
+
+cat setup.py ^
+  | sed -e "s@JPEG_ROOT = None@JPEG_ROOT = \"%LIB_PATH%\", \"%INCLUDE_PATH%\"@g" ^
+        -e "s@ZLIB_ROOT = None@ZLIB_ROOT = \"%LIB_PATH%\", \"%INCLUDE_PATH%\"@g" ^
+        -e "s@TIFF_ROOT = None@TIFF_ROOT = \"%LIB_PATH%\", \"%INCLUDE_PATH%\"@g" ^
+        -e "s@FREETYPE_ROOT = None@FREETYPE_ROOT = \"%LIB_PATH%\", \"%INCLUDE_PATH%\"@g" ^
+  > setup.py.patched
+if errorlevel 1 exit 1
+move setup.py.patched setup.py
+if errorlevel 1 exit 1
+
+python setup.py build_ext -c msvc
+if errorlevel 1 exit 1
+python setup.py install
+if errorlevel 1 exit 1
