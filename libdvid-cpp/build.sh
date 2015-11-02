f19f7497d7e4942ebc60d3378d72385aead2e2f3
Stuart Berg <bergs@janelia.hhmi.org>
2015-11-02 16:55:23 -0500

    Use NPY_VER and jinja templates to enforce numpy version compatibility for all recipes that compile against numpy. Also, include numpy and python version in their build strings.

--- libdvid-cpp/build.sh
+++ libdvid-cpp/build.sh
@@ -1,3 +1,16 @@
+if [[ ${NPY_VER} == '' ]]; then
+    set +x
+    1>&2 echo "*************************************************************************************"
+    1>&2 echo "Error: No numpy version specified."
+    1>&2 echo "       Please use --numpy=X.Y when invoking conda-build."
+    1>&2 echo "       For example:"
+    1>&2 echo
+    1>&2 echo "           conda build --python=2.7 --numpy=1.9 ${PKG_NAME}"
+    1>&2 echo
+    1>&2 echo "*************************************************************************************"
+    exit 1
+fi
+
 # Depending on our platform, shared libraries end with either .so or .dylib
 if [[ `uname` == 'Darwin' ]]; then
     DYLIB_EXT=dylib
