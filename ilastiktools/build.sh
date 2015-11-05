3845c6ccaa6ca97792e8cafe5c64dbd8a3b864cc
Stuart Berg <bergs@janelia.hhmi.org>
2015-11-04 23:30:12 -0500

    Simplified jinja templates for numpy versions and git hashes. Note: This assumes that conda/conda-build#662 and conda/conda-build#663 will be merged.

--- ilastiktools/build.sh
+++ ilastiktools/build.sh
@@ -1,16 +1,3 @@
-if [[ ${NPY_VER} == '' ]]; then
-    set +x
-    1>&2 echo "*************************************************************************************"
-    1>&2 echo "Error: No numpy version specified."
-    1>&2 echo "       Please use --numpy=X.Y when invoking conda-build."
-    1>&2 echo "       For example:"
-    1>&2 echo
-    1>&2 echo "           conda build --python=2.7 --numpy=1.9 ${PKG_NAME}"
-    1>&2 echo
-    1>&2 echo "*************************************************************************************"
-    exit 1
-fi
-
 export CXXFLAGS=""
 export CFLAGS=""
 export LDFLAGS=""
--- tifffile/build.sh
+++ tifffile/build.sh
@@ -1,22 +1,2 @@
 #!/bin/bash
-
-if [[ ${NPY_VER} == '' ]]; then
-    set +x
-    1>&2 echo "*************************************************************************************"
-    1>&2 echo "Error: No numpy version specified."
-    1>&2 echo "       Please use --numpy=X.Y when invoking conda-build."
-    1>&2 echo "       For example:"
-    1>&2 echo
-    1>&2 echo "           conda build --python=2.7 --numpy=1.9 ${PKG_NAME}"
-    1>&2 echo
-    1>&2 echo "*************************************************************************************"
-    exit 1
-fi
-
 $PYTHON setup.py install
-
-# Add more build steps here, if they are necessary.
-
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
