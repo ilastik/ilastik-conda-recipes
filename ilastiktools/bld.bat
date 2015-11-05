1b48aec367b55a22a510cf2b286808274fe26338
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2015-11-05 16:49:34 +0100

    upgraded vigra and ilastiktools to use numpy 1.10

--- ilastiktools/bld.bat
+++ ilastiktools/bld.bat
@@ -5,15 +5,12 @@ call "%TOOLSET_INFO_DIR%\common-vars.bat"
 mkdir build
 cd build
 
-%DOS_TOOLS% :to_linux_path "%LIBRARY_PREFIX%" LIBRARY_PREFIX_LINUX
-
 cmake .. -G "%CMAKE_GENERATOR%" ^
-         -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX_LINUX%" ^
-         -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX_LINUX%" ^
-         -DVIGRA_INCLUDE_DIR="%LIBRARY_PREFIX_LINUX%/include" ^
-         -DWITH_OPENMP=ON            
+         -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+         -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+         -DVIGRA_INCLUDE_DIR="%LIBRARY_PREFIX%/include" ^
+         -DWITH_OPENMP=ON
 if errorlevel 1 exit 1
-   
-set CONFIGURATION=Release
-cmake --build . --target INSTALL --config %CONFIGURATION%
+
+cmake --build . --target INSTALL --config Release
 if errorlevel 1 exit 1
