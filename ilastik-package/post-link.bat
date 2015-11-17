867d8f8733ca3ed8acc3085becb04d43ba1c531f
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2015-11-17 23:32:25 +0100

    ilastik-package: rename CONDA_DEFAULT_ENV => CONDA_ACTIVE_ENV

--- ilastik-package/post-link.bat
+++ ilastik-package/post-link.bat
@@ -1,4 +1,8 @@
-if "%CONDA_DEFAULT_ENV%"=="" (
+if "%CONDA_ACTIVE_ENV%"=="" if NOT "%CONDA_DEFAULT_ENV%"=="" (
+    set CONDA_ACTIVE_ENV=%CONDA_DEFAULT_ENV%
+)
+
+if "%CONDA_ACTIVE_ENV%"=="" (
     echo 'ilastik-package' cannot be installed into the root environment.
     echo Use 'activate environment-name' to switch to another environment.
     exit 1
