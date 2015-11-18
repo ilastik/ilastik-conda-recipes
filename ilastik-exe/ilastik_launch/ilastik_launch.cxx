e04109c29c655a4718d93c1bd66c2d250e71c672
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2015-11-18 23:01:09 +0100

    ilastik-exe: set relevant PYTHON* environment variables

--- ilastik-exe/ilastik_launch/ilastik_launch.cxx
+++ ilastik-exe/ilastik_launch/ilastik_launch.cxx
@@ -1,9 +1,9 @@
 /*  ilastik Launcher for Windows
 
     This file must reside in the ilastik root directory $ILASTIK_DIR
-    and just launches 
+    and just launches
        $ILASTIK_DIR/python/python.exe $ILASTIK_DIR/ilastik/ilastik/ilastik_launch.py args ...
-       
+
     It clears $PYTHONPATH before starting python, so that we can be sure to have
     a clean ilastik installation.
 */
@@ -13,14 +13,14 @@
 #include <vector>
 #include <string>
 #include <iostream>
- 
-void printWinError(std::string message = "") 
-{ 
+
+void printWinError(std::string message = "")
+{
     LPVOID lpMsgBuf;
-    DWORD dw = GetLastError(); 
+    DWORD dw = GetLastError();
 
     FormatMessage(
-        FORMAT_MESSAGE_ALLOCATE_BUFFER | 
+        FORMAT_MESSAGE_ALLOCATE_BUFFER |
         FORMAT_MESSAGE_FROM_SYSTEM |
         FORMAT_MESSAGE_IGNORE_INSERTS,
         NULL,
@@ -31,15 +31,15 @@ void printWinError(std::string message = "")
 
     message += (char*)lpMsgBuf;
     LocalFree(lpMsgBuf);
-    
+
     std::cerr << message << "\n";
 }
 
-int main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
     char buffer[2000];  /* the script's filename */
     GetModuleFileName(NULL, buffer, sizeof(buffer));
-    
+
     // full name of this executable
     std::string this_exe(buffer);
     // we got something like 'ilastik_root\ilastik.exe' and extract the 'ilastik_root' part
@@ -48,21 +48,23 @@ int main(int argc, char **argv)
     std::string script_name = path + "\\ilastik-meta\\ilastik\\ilastik.py";
     std::string python_home = path;
     std::string python_exe  = path + "\\python.exe";
-    
+
     std::vector<std::string> args;
     args.push_back("\"" + python_exe + "\"");
     args.push_back("\"" + script_name + "\"");
     args.push_back("\"--clean_paths\"");
     for(std::size_t k=1; k<argc; ++k)
         args.push_back(std::string("\"") + argv[k] + "\"");
-    
+
     std::vector<const char *> cargs;
     for(std::size_t k=0; k<args.size(); ++k)
         cargs.push_back(args[k].c_str());
     cargs.push_back(0);
-    
+
     SetEnvironmentVariable("PYTHONHOME", python_home.c_str());
-    
+    SetEnvironmentVariable("PYTHONPATH", "");
+    SetEnvironmentVariable("PYTHONNOUSERSITE", "1");
+
     if(_spawnv(P_WAIT, python_exe.c_str(), &cargs[0]))
         printWinError("ilastik error: \n");
 }
--- ilastik-exe/run-ilastik.bat
+++ ilastik-exe/run-ilastik.bat
@@ -26,11 +26,12 @@ set QTDIR=%INSTALL_DIR%Qt4
 IF ["%ILASTIK_DIR%"] EQU [""] set ILASTIK_DIR=%INSTALL_DIR%ilastik-meta
 set PYTHONPATH=%ILASTIK_DIR%\lazyflow;%ILASTIK_DIR%\volumina;%ILASTIK_DIR%\ilastik
 set PYTHONHOME=%INSTALL_DIR%
+set PYTHONNOUSERSITE=1
 
 rem check if this script was called as 'lastik test' or 'ilastik python ...'
 if /I "%1" EQU "test" goto :testing
 if /I "%1" EQU "python" goto :python
- 
+
 rem if not, start ilastik normally
 echo Loading ilastik from "%ILASTIK_DIR%"
 "%INSTALL_DIR%python" "%ILASTIK_DIR%\ilastik\ilastik.py" %*
