1c54280aafedaaf99e18bdd9b0d7d433bb41fea6
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2015-12-04 20:45:14 +0100

    libtiff: switched to cmake build, added lzma compression

--- libtiff/bld.bat
+++ libtiff/bld.bat
@@ -2,24 +2,24 @@ REM load toolset info
 set TOOLSET_INFO_DIR=%PREFIX%\toolset-info
 call "%TOOLSET_INFO_DIR%\common-vars.bat"
 
-%DOS_TOOLS% :to_linux_path "%LIBRARY_PREFIX%\include" INCLUDE_PATH
-%DOS_TOOLS% :to_linux_path "%LIBRARY_PREFIX%\lib" LIB_PATH
-
-rem patch nmake.opt
-cat nmake.opt ^
-  | sed -e "s@#JPEG_SUPPORT@JPEG_SUPPORT@g" ^
-        -e "s@#JPEG_INCLUDE.*@JPEG_INCLUDE = -I\"%INCLUDE_PATH%\"@g" ^
-        -e "s@#JPEG_LIB.*@JPEG_LIB = \"%LIB_PATH%/jpeg.lib\"@g" ^
-        -e "s@#ZIP_SUPPORT@ZIP_SUPPORT@g" ^
-        -e "s@#ZLIB_INCLUDE.*@ZLIB_INCLUDE = -I\"%INCLUDE_PATH%\"@g" ^
-        -e "s@#ZLIB_LIB.*@ZLIB_LIB = \"%LIB_PATH%/zlib.lib\"@g" ^
-  > nmake.opt.new
-move nmake.opt.new nmake.opt
-
-REM BUILD
-nmake /f Makefile.vc
+mkdir build
+cd build
+cmake .. -G "%CMAKE_GENERATOR%" ^
+         -Djbig=0 ^
+         -Djpeg12=0 ^
+         -Dpixarlog=0 ^
+         -Dold-jpeg=0 ^
+         -DJPEG_INCLUDE_DIR="%LIBRARY_INC%" ^
+         -DJPEG_LIBRARY="%LIBRARY_LIB%\jpeg.lib" ^
+         -DLIBLZMA_INCLUDE_DIR="%LIBRARY_INC%" ^
+         -DLIBLZMA_LIBRARY="%LIBRARY_LIB%\liblzma.lib" ^
+         -DZLIB_INCLUDE_DIR="%LIBRARY_INC%" ^
+         -DZLIB_LIBRARY="%LIBRARY_LIB%\zlib.lib" ^
+         -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"
 if errorlevel 1 exit 1
 
-REM INSTALL
-cmake -DTIFF_INSTALL_PREFIX="%LIBRARY_PREFIX%" -P "%RECIPE_DIR%/tiff_install.cmake" 
+cmake --build . --target ALL_BUILD --config Release
+if errorlevel 1 exit 1
+
+cmake --build . --target INSTALL --config Release
 if errorlevel 1 exit 1
