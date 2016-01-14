dcab1fc1c21c50e148b5ab6eef66bd62f3c40a84
Carsten Haubold <carstenhaubold@googlemail.com>
2016-01-14 15:34:41 +0100

    Explicitly link armadillo against the conda supplied openblas on linux

--- armadillo/build.sh
+++ armadillo/build.sh
@@ -1,10 +1,17 @@
 mkdir build
 cd build
+
+BLAS_STRING=""
+if [ `uname` != "Darwin" ]; then
+	BLAS_STRING="-DBLAS_LIBRARY=${PREFIX}/lib/libopenblas.so -DOpenBLAS_LIBRARY=${PREFIX}/lib/libopenblas.so -DLAPACK_LIBRARY=${PREFIX}/lib/libopenblas.so"
+fi
+
 cmake .. \
     -DCMAKE_C_COMPILER=${PREFIX}/bin/gcc \
     -DCMAKE_CXX_COMPILER=${PREFIX}/bin/g++ \
     -DCMAKE_OSX_DEPLOYMENT_TARGET=10.7 \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX}
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    ${BLAS_STRING}
 
 make -j${CPU_COUNT}
 make install
