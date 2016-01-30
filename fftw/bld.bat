2d8e4da32df375ee75970d55f014cb71fe4fdaad
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2016-01-30 02:12:58 +0100

    changed fftw recipe to use cmake

--- fftw/bld.bat
+++ fftw/bld.bat
@@ -10,19 +10,31 @@ if %ARCH%==32 (
     set FFTW_BUILD_DIR=x64
 )
 
-%PYTHON% "%RECIPE_DIR%/patch_fftw_win.py" .
 
-cd fftw-3.3-libs
+REM Use outdated solution from
+REM ftp://ftp.fftw.org/pub/fftw/fftw-3.3-libs-visual-studio-2010.zip
+REM %PYTHON% "%RECIPE_DIR%/patch_fftw_win.py" .
 
-devenv fftw-3.3-libs.sln /upgrade
-if errorlevel 1 exit 1
+REM cd fftw-3.3-libs
 
-REM BUILD
-devenv fftw-3.3-libs.sln /build %CONFIGURATION% /project libfftw-3.3
-if errorlevel 1 exit 1
-devenv fftw-3.3-libs.sln /build %CONFIGURATION% /project libfftwf-3.3
-if errorlevel 1 exit 1
+REM devenv fftw-3.3-libs.sln /upgrade
+REM if errorlevel 1 exit 1
 
-REM INSTALL
-cmake -DFFTW_BUILD_DIR="%FFTW_BUILD_DIR%" -DFFTW_INSTALL_PREFIX="%LIBRARY_PREFIX%" -P "%RECIPE_DIR%/fftw_install.cmake" 
-if errorlevel 1 exit 1
+REM REM BUILD
+REM devenv fftw-3.3-libs.sln /build %CONFIGURATION% /project libfftw-3.3
+REM if errorlevel 1 exit 1
+REM devenv fftw-3.3-libs.sln /build %CONFIGURATION% /project libfftwf-3.3
+REM if errorlevel 1 exit 1
+
+REM REM INSTALL
+REM cmake -DFFTW_BUILD_DIR="%FFTW_BUILD_DIR%" -DFFTW_INSTALL_PREFIX="%LIBRARY_PREFIX%" -P "%RECIPE_DIR%/fftw_install.cmake"
+REM if errorlevel 1 exit 1
+
+REM Use hand-made cmake
+copy "%RECIPE_DIR%\cmake\CMakeLists.txt" .
+copy "%RECIPE_DIR%\cmake\config.h" .
+
+mkdir build
+cd build
+cmake .. -G "%CMAKE_GENERATOR%" -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"
+cmake --build . --target INSTALL --config Release
