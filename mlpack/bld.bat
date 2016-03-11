3779f79a71b224a65c6b1ed0f1948a806790aff5
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2016-03-11 19:17:58 +0100

    mlpack: use armadillo 6.500.5

--- mlpack/bld.bat
+++ mlpack/bld.bat
@@ -13,11 +13,10 @@ cd build
 cmake .. -G "%CMAKE_GENERATOR%" ^
          -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX_LINUX%" ^
          -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX_LINUX%" ^
-         -DHDF5_C_LIBRARY="%LIBRARY_PREFIX_LINUX%/lib/hdf5.lib" ^
          -DICONV_LIBRARY="%LIBRARY_PREFIX_LINUX%/lib/iconv.lib" ^
          -DZLIB_LIBRARY=%LIBRARY_PREFIX_LINUX%/lib/zlib.lib"
 if errorlevel 1 exit 1
-        
+
 set CONFIGURATION=Release
 cmake --build . --target mlpack --config %CONFIGURATION%
 if errorlevel 1 exit 1
