715e1709f8c45c74a12c5432718b1afe9b93defe
Carsten Haubold <carstenhaubold@googlemail.com>
2016-04-01 15:50:46 -0400

    dpct: Build dynamic programming cell tracker without debug logging

--- dpct/build.sh
+++ dpct/build.sh
@@ -5,7 +5,8 @@ cmake .. \
     -DCMAKE_C_COMPILER=${PREFIX}/bin/gcc \
     -DCMAKE_CXX_COMPILER=${PREFIX}/bin/g++ \
     -DCMAKE_OSX_DEPLOYMENT_TARGET=10.7 \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX}
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DWITH_LOG=OFF
 
 make -j${CPU_COUNT}
 make install
