97cdbeb66a82824037d63e7ce461d2bfc20afdb4
Stuart Berg <bergs@janelia.hhmi.org>
2016-07-06 22:17:43 -0400

    build-utils/dependencies-to-json.py: - Refactor main() - Write to stdout by default - Give results in toposort order - Added options --format and --keys-only

--- build-utils/dependencies-to-json.py
+++ build-utils/dependencies-to-json.py
@@ -2,24 +2,24 @@ from __future__ import print_function
 import os
 import sys
 import json
+import logging
 import argparse
+import collections
 
 import conda.api
 import conda.install
 import conda.cli.common
 from conda.cli.main_list import list_packages
 from conda.resolve import Resolve, NoPackagesFound
+from conda.toposort import toposort
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--name', '-n')
-    parser.add_argument('--prefix', '-p')
-    parser.add_argument('--output', '-o')
-    args = parser.parse_args()
 
-    # Find the env prefix
-    prefix = conda.cli.common.get_prefix(args)
-
+def dependencies_for_env( prefix ):
+    """
+    Return an OrderedDict of all packages in the packages in the
+    given environment and each one's list of dependencies.
+    The returned items are in toposort order. 
+    """
     # Get all package strings as 'name-version-build'
     installed = conda.install.linked(prefix)
     exitcode, packages = list_packages(prefix, installed, regex=None, format='canonical', show_channel_urls=False)
@@ -34,23 +34,66 @@ def main():
     index = conda.api.get_index()
     r = Resolve(index)
     
-    json_data = {}
+    deps_dict = {}
     for package in packages:
         try:
             versions = r.get_pkgs(conda.cli.common.arg2spec(package))
         except NoPackagesFound:
-            print("Skipping " + package)
+            print("Skipping " + package, file=sys.stderr)
         else:
             for pkg in sorted(versions):
-                json_data[pkg.name] = []
+                deps_dict[pkg.name] = []
                 for dep in pkg.info['depends']:
-                    json_data[pkg.name].append(dep.split(' ')[0])
+                    deps_dict[pkg.name].append(dep.split(' ')[0])
+
+    # Convenience: Return dict with keys in topologically sorted order
+    sorted_keys = toposort( deps_dict )
+    deps_dict = collections.OrderedDict( map(lambda k: (k, deps_dict[k]), sorted_keys ) )
+    return deps_dict
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--name', '-n')
+    parser.add_argument('--prefix', '-p')
+    parser.add_argument('--output', '-o')
+    parser.add_argument('--keys-only', '-k', action='store_true')
+    parser.add_argument('--format', '-f', choices=['json', 'delimited'], default='json')
+    args = parser.parse_args()
+
+    prefix = conda.cli.common.get_prefix(args)
+
+    # If writing to stdout, make sure the logs are silent
+    if not args.output:
+        for name in ['stdoutlog', 'fetch', 'progress', 'dotupdate']:
+            logging.getLogger(name).setLevel(logging.WARN)
+    
+    deps_dict = dependencies_for_env( prefix )
+
+    if args.keys_only:
+        json_data = deps_dict.keys()
+    else:
+        json_data = deps_dict
+
+    if args.format == 'json':
+        output_text = json.dumps(json_data, sort_keys=True, indent=4, separators=(',', ': '))
+    else:
+        if isinstance(json_data, list):
+            output_text = '\n'.join(json_data)
+        else:
+            output_text = ''
+            longest_keylen = max(map(len, json_data.keys()))
+            for k,v in json_data.items():
+                output_text += k + ' '*(longest_keylen-len(k)+1) + ' '.join(v) + '\n'
 
     # Write the dict as json
     env_name = args.name or os.path.split(prefix)[-1]
-    output_filepath = args.output or (env_name + '.json')
-    with open(output_filepath, 'w') as output_file:
-        json.dump(json_data, output_file, sort_keys=True, indent=4, separators=(',', ': '))
+    if args.output:
+        with open(args.output, 'w') as output_file:
+            output_file.write( output_text )
+    else:
+        sys.stdout.write( output_text )
+
 
 if __name__ == "__main__":
     sys.exit( main() )
