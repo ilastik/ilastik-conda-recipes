aabe47ecd4e5d2c126533091d06ebfd18102dfe4
Stuart Berg <bergs@janelia.hhmi.org>
2017-01-13 16:59:18 -0500

    opengm-structured-learning-headers: On Mac, use clang

--- opengm-structured-learning-headers/build.sh
+++ opengm-structured-learning-headers/build.sh
@@ -1,8 +1,16 @@
+if [[ $(uname) == "Darwin" ]]; then
+    CC=clang
+    CXX=clang++
+else
+    CC=${PREFIX}/bin/gcc
+    CXX=${PREFIX}/bin/g++
+fi
+
 mkdir build
 cd build
 cmake ..\
-    -DCMAKE_C_COMPILER=${PREFIX}/bin/gcc \
-    -DCMAKE_CXX_COMPILER=${PREFIX}/bin/g++ \
+    -DCMAKE_C_COMPILER=${CC} \
+    -DCMAKE_CXX_COMPILER=${CXX} \
 	-DCMAKE_OSX_DEPLOYMENT_TARGET=10.7\
 	-DCMAKE_INSTALL_PREFIX=${PREFIX}\
 	-DCMAKE_PREFIX_PATH=${PREFIX}\
