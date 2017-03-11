bd64d5c15e705ea9f2e3719de96d851e4abc4eb7
Stephen Plaza <plazas@janelia.hhmi.org>
2017-03-11 16:17:45 -0500

    adds google client api recipe

---
+++ google-api-cpp-client/build.sh
@@ -0,0 +1,15 @@
+mkdir build && cd build
+cmake ..\
+        -DCMAKE_C_COMPILER="${PREFIX}/bin/gcc" \
+        -DCMAKE_CXX_COMPILER="${PREFIX}/bin/g++" \
+        -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+        -DCMAKE_PREFIX_PATH="${PREFIX}" \
+        -DCMAKE_CXX_FLAGS=-I"${PREFIX}/include" \
+        -Dgoogleapis_build_samples=OFF \
+        -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
+        -DCMAKE_EXE_LINKER_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
+
+make -j${CPU_COUNT} all
+
+make install
+
