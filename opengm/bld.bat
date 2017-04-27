7da1e305e7619906f75ff71302aa6bd92dbfeb85
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2017-04-27 19:51:24 +0200

    upgrades for ilastik 1.2.1

--- opengm/bld.bat
+++ opengm/bld.bat
@@ -7,28 +7,29 @@ cd build
 
 %DOS_TOOLS% :to_linux_path "%LIBRARY_PREFIX%" LIBRARY_PREFIX_LINUX
 
-REM uncomment to compile Python bindings
-REM cmake .. -G "%CMAKE_GENERATOR%" ^
-         REM -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX_LINUX%" ^
-         REM -DBUILD_PYTHON_WRAPPER=ON ^
-         REM -DPYTHON_LIBRARY="%PREFIX%\libs\python27.lib" ^
-         REM -DPYTHON_INCLUDE_DIR="%PREFIX%\include" ^
-         REM -DBUILD_EXAMPLES=OFF ^
-         REM -DBUILD_TUTORIALS=OFF ^
-         REM -DBUILD_TESTING=OFF ^
-         REM -DWITH_BOOST=ON ^
-         REM -DWITH_CPLEX=ON ^
-         REM -DWITH_HDF5=ON
 cmake .. -G "%CMAKE_GENERATOR%" ^
          -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX_LINUX%" ^
+         -DBUILD_PYTHON_WRAPPER=ON ^
+         -DPYTHON_LIBRARY="%PREFIX%\libs\python27.lib" ^
+         -DPYTHON_INCLUDE_DIR="%PREFIX%\include" ^
+         -DPYTHON_INSTALL_DIR="%PREFIX%\Lib\site-packages\opengm" ^
          -DBUILD_EXAMPLES=OFF ^
          -DBUILD_TUTORIALS=OFF ^
          -DBUILD_TESTING=OFF ^
+         -DWITH_VIGRA=ON ^
          -DWITH_BOOST=ON ^
-         -DWITH_CPLEX=ON ^
+         -DWITH_CPLEX=OFF ^
          -DWITH_HDF5=ON
 if errorlevel 1 exit 1
-    
-set CONFIGURATION=Release
-cmake --build . --target INSTALL --config %CONFIGURATION%
+
+cmake --build . --target externalLibs
+if errorlevel 1 exit 1
+
+cmake . -DWITH_MAXFLOW=ON -DWITH_MAXFLOW_IBFS=ON -DWITH_QPBO=ON
+if errorlevel 1 exit 1
+
+cmake --build . --target ALL_BUILD --config Release
+if errorlevel 1 exit 1
+
+cmake --build . --target INSTALL --config Release
 if errorlevel 1 exit 1
