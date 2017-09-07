133d69f4faeddbb7383a65aaa6905989f911ac3b
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2017-09-07 17:07:12 +0200

    upgraded ilastiktools

--- ilastiktools/bld.bat
+++ ilastiktools/bld.bat
@@ -9,7 +9,10 @@ cmake .. -G "%CMAKE_GENERATOR%" ^
          -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
          -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
          -DVIGRA_INCLUDE_DIR="%LIBRARY_PREFIX%/include" ^
-         -DWITH_OPENMP=ON
+         -DWITH_OPENMP=ON ^
+         -DPYTHON_INCLUDE_DIRS="%PREFIX%\include" ^
+         -DPYTHON_LIBRARIES="%PREFIX%\libs\python27.lib" ^
+         -DPYTHON_LIBRARY="%PREFIX%\libs\python27.lib"
 if errorlevel 1 exit 1
 
 cmake --build . --target INSTALL --config Release
