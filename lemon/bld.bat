39ca700259071f85d47766c1cdb41f2ae761666c
Carsten Haubold <carsten.haubold@iwr.uni-heidelberg.de>
2017-09-07 05:56:04 -0700

    Add windows bld.bat for lemon

---
+++ lemon/bld.bat
@@ -0,0 +1,12 @@
+mkdir build
+cd build
+
+set CONFIGURATION=Release
+
+cmake .. -G "%CMAKE_GENERATOR%" -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" 
+REM -DGLPK_LIBRARY="" -DGLPK_INCLUDE_DIR="" -DGLPK_ROOT_DIR=""
+if errorlevel 1 exit 1
+cmake --build . --target ALL_BUILD --config %CONFIGURATION%
+if errorlevel 1 exit 1
+cmake --build . --target INSTALL --config %CONFIGURATION%
+if errorlevel 1 exit 1
