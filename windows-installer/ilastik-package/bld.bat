288127c3a599c3bef7e1cce76962816527168bf3
Carsten Haubold <carsten.haubold@iwr.uni-heidelberg.de>
2017-10-19 15:13:25 +0200

    Add windows installer package

---
+++ windows-installer/ilastik-package/bld.bat
@@ -0,0 +1,7 @@
+if not exist "%PREFIX%\package" mkdir "%PREFIX%\package"
+
+copy "%RECIPE_DIR%\ilastik.iss.in" "%PREFIX%\package"
+copy "%RECIPE_DIR%\ilastik-icon.ico" "%PREFIX%\package"
+copy "%RECIPE_DIR%\ilastik-installer-small.bmp" "%PREFIX%\package"
+copy "%RECIPE_DIR%\ilastik-installer.bmp" "%PREFIX%\package"
+copy "%RECIPE_DIR%\LICENSE.txt" "%PREFIX%"
---
+++ windows-installer/ilastik-package/post-link.bat
@@ -0,0 +1,22 @@
+if "%CONDA_ACTIVE_ENV%"=="" if NOT "%CONDA_DEFAULT_ENV%"=="" (
+    set CONDA_ACTIVE_ENV=%CONDA_DEFAULT_ENV%
+)
+
+if "%CONDA_ACTIVE_ENV%"=="" (
+    echo 'ilastik-package' cannot be installed into the root environment.
+    echo Use 'activate environment-name' to switch to another environment.
+    exit 1
+)
+
+if not exist tmp mkdir tmp
+copy "%RECIPE_DIR%\read-version.py" tmp
+
+python tmp\read-version.py "%PREFIX%/ilastik-meta/ilastik/ilastik/__init__.py" > tmp\version.txt
+set /p ILASTIK_PKG_VERSION=<tmp\version.txt
+
+cd "%PREFIX%\package"
+
+REM copy the ilastik version as our package version
+cat ilastik.iss.in ^
+    | sed -e "s/@VERSION@/%ILASTIK_PKG_VERSION%/g" ^
+    > ilastik.iss
