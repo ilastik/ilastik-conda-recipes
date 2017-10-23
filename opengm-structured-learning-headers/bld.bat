c8de33ff4d4c2bf18bac94ec79a0fc8b1e99bf7e
Carsten Haubold <carsten.haubold@iwr.uni-heidelberg.de>
2017-10-23 15:19:01 +0200

    fix windows packe for opengm-structured-learning, was empty before

---
+++ opengm-structured-learning-headers/bld.bat
@@ -0,0 +1,11 @@
+mkdir build
+cd build
+
+set CONFIGURATION=Release
+
+cmake .. -G "%CMAKE_GENERATOR%" -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" -DWITH_BOOST=ON -DWITH_HDF5=ON -DBUILD_PYTHON_WRAPPER=OFF -DBUILD_TESTING=OFF -DBUILD_TUTORIALS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_COMMANDLINE=OFF
+if errorlevel 1 exit 1
+cmake --build . --target ALL_BUILD --config %CONFIGURATION%
+if errorlevel 1 exit 1
+cmake --build . --target INSTALL --config %CONFIGURATION%
+if errorlevel 1 exit 1
