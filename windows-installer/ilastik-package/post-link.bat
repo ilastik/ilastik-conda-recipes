948118ea44013da52bed71aaddce8eedb36f9aab
Fynn Beuttenmueller <fynn.beuttenmueller@iwr.uni-heidelberg.de>
2017-12-22 11:28:42 +0100

    remove undefined %RECIPE_DIR% and use relativ paths instead
    (same for %PREFIX%, although that was not the issue)

--- windows-installer/ilastik-package/post-link.bat
+++ windows-installer/ilastik-package/post-link.bat
@@ -1,23 +1,22 @@
-if "%CONDA_ACTIVE_ENV%"=="" if NOT "%CONDA_DEFAULT_ENV%"=="" (
+@if "%CONDA_ACTIVE_ENV%"=="" if NOT "%CONDA_DEFAULT_ENV%"=="" (
     set CONDA_ACTIVE_ENV=%CONDA_DEFAULT_ENV%
 )
 
-if "%CONDA_ACTIVE_ENV%"=="" (
+@if "%CONDA_ACTIVE_ENV%"=="" (
     echo 'ilastik-package' cannot be installed into the root environment.
     echo Use 'activate environment-name' to switch to another environment.
     exit 1
 )
 
-if not exist tmp mkdir tmp
-copy "%RECIPE_DIR%\read-version.py" tmp
+for /f %%i in (
+    'python -c "import re;s = open('..\ilastik-meta\ilastik\ilastik\__init__.py').read();version_line = re.findall(r'__version_info__ *=.*', s)[0];exec(version_line);print('.'.join(map(str,__version_info__)))"'
+) do set ILASTIK_PKG_VERSION=%%i
 
-python tmp\read-version.py "%PREFIX%\ilastik-meta\ilastik\ilastik\__init__.py" > tmp\version.txt
-set /p ILASTIK_PKG_VERSION=<tmp\version.txt
-echo "Building package for version %ILASTIK_PKG_VERSION%"
+@echo "Building package for version %ILASTIK_PKG_VERSION%"
 
-cd "%PREFIX%\package"
+cd ../package
 
-REM copy the ilastik version as our package version
-cat ilastik.iss.in ^
+@rem copy the ilastik version as our package version
+cat "ilastik.iss.in" ^
     | sed -e "s/@VERSION@/%ILASTIK_PKG_VERSION%/g" ^
-    > ilastik.iss
+    > "ilastik.iss"
