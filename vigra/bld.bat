eb752da56b577514d4f328d740d69629cf755a45
k-dominik <k-dominik@users.noreply.github.com>
2018-12-10 14:07:34 +0100

    updated conda recipe to use nmake

--- vigra/bld.bat
+++ vigra/bld.bat
@@ -4,23 +4,42 @@ cd build
 set CONFIGURATION=Release
 set PATH=%PATH%;%LIBRARY_PREFIX%\bin
 
-cmake .. -G "%CMAKE_GENERATOR%" -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" -DCMAKE_CXX_FLAGS="-DH5_BUILT_AS_DYNAMIC_LIB /EHsc -DFFTW_DLL -DBOOST_ALL_NO_LIB" -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" -DWITH_LEMON=1 -DPYTHON_EXECUTABLE="%PYTHON%" -DBUILD_SHARED_LIBS=1
+cmake .. -G "NMake Makefiles" ^
+    -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+    -DCMAKE_BUILD_TYPE=%CONFIGURATION% ^
+    -DTEST_VIGRANUMPY=1 ^
+    -DAUTOEXEC_TESTS=0 ^
+    -DCMAKE_CXX_FLAGS="-DH5_BUILT_AS_DYNAMIC_LIB /EHsc -DFFTW_DLL -DBOOST_ALL_NO_LIB" ^
+    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+    -DWITH_LEMON=1 ^
+    -DPYTHON_EXECUTABLE="%PYTHON%" ^
+    -DBUILD_SHARED_LIBS=1
 if errorlevel 1 exit 1
 
 REM BUILD
-cmake --build . --target ALL_BUILD --config %CONFIGURATION%
+nmake check
 if errorlevel 1 exit 1
 
 REM TEST
-cmake --build . --target test_impex --config %CONFIGURATION%
-if errorlevel 1 exit 1
-cmake --build . --target test_hdf5impex --config %CONFIGURATION%
-if errorlevel 1 exit 1
-cmake --build . --target test_fourier --config %CONFIGURATION%
-if errorlevel 1 exit 1
-cmake --build . --target vigranumpytest --config %CONFIGURATION%
+ctest -V
 if errorlevel 1 exit 1
 
-REM INSTALL
-cmake --build . --target INSTALL --config %CONFIGURATION%
+nmake check_python
 if errorlevel 1 exit 1
+
+nmake install
+if errorlevel 1 exit 1
+
+rem REM TEST
+rem cmake --build . --target test_impex --config %CONFIGURATION%
+rem if errorlevel 1 exit 1
+rem cmake --build . --target test_hdf5impex --config %CONFIGURATION%
+rem if errorlevel 1 exit 1
+rem cmake --build . --target test_fourier --config %CONFIGURATION%
+rem if errorlevel 1 exit 1
+rem cmake --build . --target vigranumpytest --config %CONFIGURATION%
+rem if errorlevel 1 exit 1
+
+rem REM INSTALL
+rem cmake --build . --target INSTALL --config %CONFIGURATION%
+rem if errorlevel 1 exit 1
