65bbc24366f1815eb3de272162234cdb9b401970
k-dominik <k-dominik@users.noreply.github.com>
2019-01-25 16:34:37 +0100

    opengm-structured-learning-headers: compiler switch, cleanup

--- opengm-structured-learning-headers/build.sh
+++ opengm-structured-learning-headers/build.sh
@@ -1,8 +1,6 @@
 mkdir build
 cd build
 cmake ..\
-    -DCMAKE_C_COMPILER=${PREFIX}/bin/gcc \
-    -DCMAKE_CXX_COMPILER=${PREFIX}/bin/g++ \
 	-DCMAKE_OSX_DEPLOYMENT_TARGET=10.7\
 	-DCMAKE_INSTALL_PREFIX=${PREFIX}\
 	-DCMAKE_PREFIX_PATH=${PREFIX}\
