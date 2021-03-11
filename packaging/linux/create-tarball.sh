6ecdfa741c64249825598041c0e1538d49b60345
k-dominik <k-dominik@users.noreply.github.com>
2021-03-11 18:19:57 +0100

    removed unused options, prepared to move env creation outside script
    All options that have not been used in the past 4 years have been removed.
    
     * skip-tar: I prefer testing the release on a different machine, so moving it
       requires compression anyway
     * git-latest: any release we make should have a tag in the repo
     * no-solvers: for distribution we always include support for tracking with learning

--- packaging/linux/create-tarball.sh
+++ packaging/linux/create-tarball.sh
@@ -1,43 +1,10 @@
 #!/bin/bash
 
 ##
-## Usage: create-tarball.sh [--skip-tar] [--git-latest] [--no-solvers] [--include-tests] [... extra install-args, e.g. --use-local or -c ilastik-forge ...]
+## Usage: create-tarball.sh[--include-tests] release-name
 ##
 
 set -e
-
-SKIP_TAR=0
-if [[ $@ == *"--skip-tar"* ]]; then
-    if [[ $1 == "--skip-tar" ]]; then
-        SKIP_TAR=1
-       shift
-    else
-        echo "Error: --skip-tar may only be provided as the first arg." >&2
-        exit 1
-    fi
-fi
-
-USE_GIT_LATEST=0
-if [[ $@ == *"--git-latest"* ]]; then
-    if [[ $1 == "--git-latest" ]]; then
-        USE_GIT_LATEST=1
-       shift
-    else
-        echo "Error: --git-latest may only be provided as the first arg (or after --skip-tar)." >&2
-        exit 1
-    fi
-fi
-
-export WITH_SOLVERS=1
-if [[ $@ == *"--no-solvers"* ]]; then
-    if [[ $1 == "--no-solvers" ]]; then
-        export WITH_SOLVERS=0
-        shift
-    else
-        echo "Error: --no-solvers may only be provided as the first arg (or after --git-latest)." >&2
-        exit 1
-    fi
-fi
 
 INCLUDE_TESTS=0
 if [[ $@ == *"--include-tests"* ]]; then
@@ -50,57 +17,16 @@ if [[ $@ == *"--include-tests"* ]]; then
     fi
 fi
 
+
+RELEASE_NAME=$1
+
 CONDA_ROOT=`conda info --root`
 source ${CONDA_ROOT}/bin/activate root
 
-# Remove old ilastik-release environment
-if [ -d ${CONDA_ROOT}/envs/ilastik-release ]; then
-    echo "Removing old ilastik-release environment..."
-    conda remove -y --all -n ilastik-release
-fi
+RELEASE_ENV=${CONDA_ROOT}/envs/ilastik-release
 
-function latest_build()
-{
-    VERSION_AND_BUILD=$(conda search -f $@ \
-                        | tail -n1 \
-                        | python -c 'import sys; print("=".join(sys.stdin.read().split()[:2]))')
-    echo "$VERSION_AND_BUILD"
-}
 
-# Create new ilastik-release environment and install all ilastik dependencies to it.
-if [[ $WITH_SOLVERS == 0 ]]; then
-    EVERYTHING_PKG=$(latest_build ilastik-dependencies-binary-no-solvers "$@")
-    SOLVERS_SUFFIX="-no-solvers"
-else    
-    EVERYTHING_PKG=$(latest_build ilastik-dependencies-binary "$@")
-    SOLVERS_SUFFIX=""
-fi
-
-echo "Creating new ilastik-release environment using ${EVERYTHING_PKG}"
-conda create -q -y -n ilastik-release ${EVERYTHING_PKG} --override-channels "$@"
-
-if [[ $USE_GIT_LATEST == 1 ]]; then
-    # Instead of keeping the version from binstar, get the git repo
-    ILASTIK_META=${CONDA_ROOT}/envs/ilastik-release/ilastik-meta
-    rm -rf ${ILASTIK_META}
-
-    echo "Cloning ilastik from latest github sources"
-    git clone https://github.com/ilastik/ilastik-meta ${ILASTIK_META}
-    cd ${ILASTIK_META}
-    git submodule init
-    git submodule update
-    git submodule foreach 'git checkout master'
-
-    echo "Compiling python sources"
-    python -m compileall lazyflow volumina ilastik
-    cd -
-    ILASTIK_PKG_VERSION="master"
-else
-    # Ask conda for the package version
-    ILASTIK_PKG_VERSION=`conda list -n ilastik-release | grep ilastik-meta | python -c "import sys; print(sys.stdin.read().split()[1])"`
-fi
-
-RELEASE_NAME=ilastik-${ILASTIK_PKG_VERSION}${SOLVERS_SUFFIX}-`uname`
+echo "Creating release ${RELEASE_NAME} from ${RELEASE_ENV}"
 
 # Remove cplex libs/symlinks (if present)
 rm -f ${CONDA_ROOT}/envs/ilastik-release/lib/libcplex.so
