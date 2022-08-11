b00eebf3944930485e2799b4bb4d4e483f194f57
Lucille Delisle <lucille.delisle@epfl.ch>
2022-08-11 12:29:50 +0200

    update test to ensure ilastik path is set

--- recipes/fiji-ilastik/run_test.sh
+++ recipes/fiji-ilastik/run_test.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-ImageJ --ij2 --headless --console --run test_ilastik.groovy 'ilastik_project="2dcellsdemo.ilp",input_image="2d_cells_apoptotic_1channel.png",output_h5="2d_cells_apoptotic_1channel_Probabilities.h5"'
+ImageJ-ilastik --ij2 --headless --console --run test_ilastik.groovy 'ilastik_project="2dcellsdemo.ilp",input_image="2d_cells_apoptotic_1channel.png",output_h5="2d_cells_apoptotic_1channel_Probabilities.h5"'
 echo PASS
