c8a00498e19f9d63653c91617e0dec7cdfe45b59
k-dominik <k-dominik@users.noreply.github.com>
2023-02-16 17:13:01 +0100

    updated windows installer images/icon

--- recipes/windows-installer/ilastik-package/bld.bat
+++ recipes/windows-installer/ilastik-package/bld.bat
@@ -2,6 +2,5 @@ if not exist "%PREFIX%\package" mkdir "%PREFIX%\package"
 
 copy "%RECIPE_DIR%\ilastik.iss.in" "%PREFIX%\package"
 copy "%RECIPE_DIR%\ilastik-icon.ico" "%PREFIX%\package"
-copy "%RECIPE_DIR%\ilastik-installer-small.bmp" "%PREFIX%\package"
-copy "%RECIPE_DIR%\ilastik-installer.bmp" "%PREFIX%\package"
+copy "%RECIPE_DIR%\ilastik-installer-*.bmp" "%PREFIX%\package"
 copy "%RECIPE_DIR%\LICENSE.txt" "%PREFIX%"
